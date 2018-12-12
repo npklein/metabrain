b8bf3a983bd3b73e6b0634b8496b7a5ea7d92e78
npklein <niekdeklein@gmail.com>
2018-12-12 14:53:12 +0100

    write samples that are missed

--- ENA/ENA_samplesheets/filter_ENA_samplesheet.py
+++ ENA/ENA_samplesheets/filter_ENA_samplesheet.py
@@ -15,6 +15,8 @@ for f in glob.glob('../brain_samples/*txt'):
             samples_to_keep.add(line)
 
 today = datetime.datetime.today().strftime('%Y%m%d')
+samples_added = set([])
+x = 0
 with open('ena.txt') as input_file, open('samplesheet_ENA_'+today+'.txt','w') as out:
     out.write(input_file.readline())
     for line in input_file:
@@ -22,3 +24,16 @@ with open('ena.txt') as input_file, open('samplesheet_ENA_'+today+'.txt','w') as
         run_accession = split_line[5]
         if run_accession in samples_to_keep:
             out.write(line)
+            samples_added.add(run_accession)
+            x += 1
+
+print('Added',str(x),'of',str(len(samples_to_keep)),'samples')
+samples_missed = samples_to_keep-samples_added
+
+if len(samples_missed) > 0:
+    print('Missed samples:')
+    print('\n'.join(samples_missed))
+    with open('missing_samples_'+today+'.txt','w') as out:
+        out.write('# These samples are in the sample list, but not in the ENA samplesheet. Possibly removed\n')
+        for sample in samples_missed:
+            out.write(sample+'\n')
