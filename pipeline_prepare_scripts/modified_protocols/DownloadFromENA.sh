60bc9cc48abcd2452f435d19033b786b7341161a
npklein <niekdeklein@gmail.com>
2018-12-12 16:42:55 +0100

    add correct variables

--- pipeline_prepare_scripts/modified_protocols/DownloadFromENA.sh
+++ pipeline_prepare_scripts/modified_protocols/DownloadFromENA.sh
@@ -9,6 +9,8 @@
 #string stage
 #string checkStage
 #string brainDir
+#string enaSamplesheet
+#string reads1FqGz
 
 #Load modules
 ${stage} Python/${pythonVersion}
@@ -20,15 +22,17 @@ echo "## "$(date)" Start $0"
 echo "ID (internalId-project-sampleName): ${internalId}-${project}-${sampleName}"
 
 
-python $brainDir/ENA/download_ENA_samples.py
+python $brainDir/ENA/download_ENA_samples.py ${enaSamplesheet} \
+                                             $(dirname ${reads1FqGz}) \
+                                             --sample ${sampleName}
 
 returnCode=$?
 echo "returncode: $returnCode";
 if [ $returnCode -eq 0 ]
 then
-  echo "convert CRAM to BAM successful"
+  echo "Downloading fastq file successful"
 else
-  echo "ERROR: couldn't convert to CRAM to BAM"
+  echo "ERROR: Could not download fastq file"
   exit 1;
 fi
 
