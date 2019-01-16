389eaf4e488549fdd353a953a3d55ed759d18bc4
npklein <niekdeklein@gmail.com>
2019-01-16 14:17:44 +0100

    add new GATK protocols (to use GATK4 instead of GATK3)

---
+++ pipeline_prepare_scripts/modified_protocols/GatkAnalyseCovariates.sh
@@ -0,0 +1,83 @@
+#MOLGENIS nodes=1 ppn=1 mem=12gb walltime=23:59:00
+
+### variables to help adding to database (have to use weave)
+#string sampleName
+#string project
+###
+#string stage
+#string checkStage
+#string RVersion
+#string gatkVersion
+#string onekgGenomeFasta
+#string goldStandardVcf
+#string goldStandardVcfIdx
+#string oneKgPhase1IndelsVcf
+#string oneKgPhase1IndelsVcfIdx
+#string dbsnpVcf
+#string dbsnpVcfIdx
+#string bqsrDir
+#string bqsrBam
+#string bqsrBai
+#string analyseCovarsDir
+#string bqsrBeforeGrp
+#string bqsrAfterGrp
+#string analyseCovariatesPdf
+#string toolDir
+#string analyseCovariatesIntermediateCsv
+
+echo "## "$(date)" Start $0"
+
+
+
+${stage} R/${RVersion}
+${stage} GATK/${gatkVersion}
+${checkStage}
+
+mkdir -p ${analyseCovarsDir}
+
+#do bqsr for covariable determination then do print reads for valid bqsrbams
+#check the bqsr part and add known variants
+
+$EBROOTGATK/gatk BaseRecalibrator\
+ -R ${onekgGenomeFasta} \
+ -I ${bqsrBam} \
+ -o ${bqsrAfterGrp} \
+ -knownSites ${dbsnpVcf} \
+ -knownSites ${goldStandardVcf} \
+ -knownSites ${oneKgPhase1IndelsVcf} \
+ --TMP_DIR=$TMPDIR
+
+returnCode=$?
+echo "returncode: $returnCode";
+if [ $returnCode -eq 0 ]
+then
+  echo "BaseCalibrator succesful"
+else
+  echo "ERROR: Could not calibrate bases"
+  exit 1;
+fi
+
+$EBROOTGATK/gatk \
+ -T AnalyzeCovariates \
+ -R ${onekgGenomeFasta} \
+ -ignoreLMT \
+ -before ${bqsrBeforeGrp} \
+ -after ${bqsrAfterGrp} \
+ -l DEBUG \
+ -csv ${analyseCovariatesIntermediateCsv} \
+ -plots ${analyseCovariatesPdf} \
+ --TMP_DIR=$TMPDIR
+
+returnCode=$?
+echo "returncode: $returnCode";
+if [ $returnCode -eq 0 ]
+then
+  echo "AnalyzeCovariates succesful"
+else
+  echo "ERROR: Could not analyze covariates"
+  exit 1;
+fi
+
+echo "returncode: $?";
+
+echo "## "$(date)" ##  $0 Done "
---
+++ pipeline_prepare_scripts/modified_protocols/GatkHaplotypeCallerGvcf.sh
@@ -0,0 +1,78 @@
+#MOLGENIS walltime=23:59:00 mem=14gb ppn=1
+
+### variables to help adding to database (have to use weave)
+#string sampleName
+#string project
+###
+#string stage
+#string checkStage
+
+#string WORKDIR
+#string projectDir
+#string gatkVersion
+#string dbsnpVcf
+#string dbsnpVcfIdx
+#string onekgGenomeFasta
+#string genomeBuild
+#string resDir
+#string referenceFastaName
+#list bqsrBam
+#string haplotyperDir
+#string toolDir
+#string haplotyperTargetsPrefix
+#string haplotyperTargetsPostfix
+
+echo "## "$(date)" Start $0"
+
+#Load gatk module
+${stage} GATK/${gatkVersion}
+${checkStage}
+
+#sort unique and print like 'INPUT=file1.bam INPUT=file2.bam '
+bams=($(printf '%s\n' "${bqsrBam[@]}" | sort -u ))
+
+inputs=$(printf ' -I %s ' $(printf '%s\n' ${bams[@]}))
+
+mkdir -p ${haplotyperDir}
+
+#do variant calling for all 25 chromosomes seperate
+#for this purpose haplotyperGvcf variable is split in ${haplotyperDir}${sampleName}.chr$CHR.g.vcf
+
+for CHR in {1..25}
+do
+   echo "CHR $CHR"
+   $EBROOTGATK/gatk HaplotypeCaller \
+       -R ${onekgGenomeFasta} \
+       --dbsnp ${dbsnpVcf} \
+       $inputs \
+       -dontUseSoftClippedBases \
+       -stand_call_conf 10.0 \
+       -stand_emit_conf 20.0 \
+       -o ${haplotyperDir}${sampleName}.chr$CHR.g.vcf.gz \
+       -variant_index_type LINEAR \
+       -variant_index_parameter 128000 \
+       -L ${haplotyperTargetsPrefix}$CHR${haplotyperTargetsPostfix} \
+       --emitRefConfidence GVCF \
+       --TMP_DIR=$TMPDIR
+
+       returnCode=$?
+       echo "returncode: $returnCode";
+       if [ $returnCode -eq 0 ]
+       then
+          echo "Gatk succesful for chr $CHR"
+       else
+          echo "ERROR: GATK not successful for chr $CHR"
+          exit 1;
+       fi
+
+    echo "returncode: $?";
+    #haplotyperGvcf is split into seperate variables now
+
+    cd ${haplotyperDir}
+	md5sum $(basename ${haplotyperDir}${sampleName}.chr$CHR.g.vcf.gz)> $(basename ${haplotyperDir}${sampleName}.chr$CHR.g.vcf.gz).md5sum
+    cd -
+
+done
+
+
+echo "## "$(date)" ##  $0 Done "
