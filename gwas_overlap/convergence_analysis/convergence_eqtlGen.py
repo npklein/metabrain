735b7fc2013b457e9a3a66f37ce3dda796b9dbf5
npklein <niekdeklein@gmail.com>
2019-01-23 09:11:37 +0100

    add gwas overlap and convergence analysis

---
+++ gwas_overlap/convergence_analysis/convergence_eqtlGen.py
@@ -0,0 +1,5 @@
+import glob
+
+# TODO: change this to command line input
+for f in glob.glob('/groups/umcg-biogen/tmp03/umcg-ndeklein/gwas_overlap/biogen-cisandtrans-4neurodiseases/eqtlgen_disease_overlap/*trans*'):
+    print(f)
