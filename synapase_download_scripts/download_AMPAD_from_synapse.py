bda2cd2f1e0d44a374bf5c94b5748079d427c111
npklein <niekdeklein@gmail.com>
2019-03-14 16:58:08 +0100

    update download scripts

--- synapase_download_scripts/download_AMPAD_from_synapse.py
+++ synapase_download_scripts/download_AMPAD_from_synapse.py
@@ -1,11 +1,9 @@
-import os
 import synapseclient
-import synapseutils
+import argparse
 import getpass
+import synapseutils
 
 parser = argparse.ArgumentParser(description='Download RNAseq and genotypes of CMC.')
-parser.add_argument('RNAseq_directory', help='Directory to download RNAseq data to')
-parser.add_argument('Genotype_directory', help='Directory to download genotypes to')
 
 args = parser.parse_args()
 
@@ -20,9 +18,21 @@ syn.login(user,password)
 print('Sync AMP-AD')
 
 # STAR MSBB
-files = synapseutils.syncFromSynapse(syn, 'syn12104381', path = 'MSBB/STAR')#, exclude='.bam')
+# files = synapseutils.syncFromSynapse(syn, 'syn12104381', path = 'MSBB/STAR')#, exclude='.bam')
+# metadata MSBB
+files = synapseutils.syncFromSynapse(syn, 'syn7392158', path = 'metadata/')#, exclude='.bam')
+
 # STAR MAYO
-files = synapseutils.syncFromSynapse(syn, 'syn12104376', path = 'MAYO/STAR/')#, exclude='.bam')
+#files = synapseutils.syncFromSynapse(syn, 'syn12104376', path = 'MAYO/STAR/')#, exclude='.bam')
+# metadata MAYO
+files = synapseutils.syncFromSynapse(syn, 'syn11384571', path = 'metadata/')#, exclude='.bam')
+files = synapseutils.syncFromSynapse(syn, 'syn5223705', path = 'metadata/')#, exclude='.bam')
+files = synapseutils.syncFromSynapse(syn, 'syn3817650', path = 'metadata/')#, exclude='.bam')
+
 # STAR ROSMAP
-files = synapseutils.syncFromSynapse(syn, 'syn12104384', path = 'ROSMAP/STAR/')#, exclude='.bam')
+#files = synapseutils.syncFromSynapse(syn, 'syn12104384', path = 'ROSMAP/STAR/')#, exclude='.bam')
+# ROSMAP metadata
+files = synapseutils.syncFromSynapse(syn, 'syn3157322', path = 'metadata')#, exclude='.bam')
+files = synapseutils.syncFromSynapse(syn, 'syn11958660', path = 'metadata')#, exclude='.bam')
+files = synapseutils.syncFromSynapse(syn, 'syn11384589', path = 'metadata')#, exclude='.bam')
 
