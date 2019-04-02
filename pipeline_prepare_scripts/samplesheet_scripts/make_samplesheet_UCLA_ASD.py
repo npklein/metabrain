f5cbdfb6ead2121d33ab9e474a65588641046019
npklein <niekdeklein@gamail.com>
2019-04-02 13:47:15 +0200

    commit before mrege

--- pipeline_prepare_scripts/samplesheet_scripts/make_samplesheet_UCLA_ASD.py
+++ pipeline_prepare_scripts/samplesheet_scripts/make_samplesheet_UCLA_ASD.py
@@ -19,9 +19,18 @@ batch_count = {}
 batch_size = 25
 batch_number = 0
 out = None
+fastq_files_initial = set(glob.glob(args.fastq_dir+'/*'))
+fastq_files = set([])
+for f in fastq_files_initial:
+    while '//' in f:
+        fastq_files.replace('//','/')
+    fastq_files.add(f)
+
 with open(args.samplesheet) as input_file:
     header = input_file.readline().split(',')
     for index, line in enumerate(input_file):
+        if 'UMB5308' in line:
+            print(line)
         if len(line.split(',')[1].strip()) == 0:
             continue
         if index % 25 == 0:
@@ -35,6 +44,31 @@ with open(args.samplesheet) as input_file:
         sample = line[2].replace('Sample_','').replace('_1st','').replace('_2nd','').replace('_3rd','')
         R1 = args.fastq_dir+'/'+sample+'.R1.fastq.gz'
         R2 = args.fastq_dir+'/'+sample+'.R2.fastq.gz'
-
+        if not os.path.isfile(R1):
+            raise RuntimeError(R1+' does not exist')
+        if not os.path.isfile(R2):
+            raise RuntimeError(R2+' does not exist')
         out.write(sample+',UCLA_ASD,'+individual+','+R1+','+R2+'\n')
+
+        while '//' in R1:
+            R1 = R1.replace('//','/')
+        try:
+            fastq_files.remove(R1)
+        except KeyError:
+            if '1st' in line[2] or '2nd' in line[2] or '3rd' in line[2]:
+                pass
+            else:
+                raise
+        while '//' in R2:
+            R2 = R2.replace('//','/')
+        try:
+            fastq_files.remove(R2)
+        except KeyError:
+            if '1st' in line[2] or '2nd' in line[2] or '3rd' in line[2]:
+                pass
+            else:
+                raise
+
 out.close()
+print('Missing samples:')
+print(fastq_files)
