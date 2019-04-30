0841f3e4aa8b7b24392d5a610383dea041599a2e
npklein <niekdeklein@gmail.com>
2019-04-30 17:47:39 +0200

    commit before merge

--- synapse_download_scripts/download_AMPAD_from_synapse.py
+++ synapse_download_scripts/download_AMPAD_from_synapse.py
@@ -21,9 +21,15 @@ print('Sync AMP-AD')
 # files = synapseutils.syncFromSynapse(syn, 'syn12104381', path = 'MSBB/STAR')#, exclude='.bam')
 # metadata MSBB
 files = synapseutils.syncFromSynapse(syn, 'syn7392158', path = 'metadata/')#, exclude='.bam')
-
+# aligned BAM files MSBB
+files = synapseutils.syncFromSynapse(syn, 'syn8540822', path = 'BAMs/MSBB/')#, exclude='.bam')
+exit()
 # STAR MAYO
 #files = synapseutils.syncFromSynapse(syn, 'syn12104376', path = 'MAYO/STAR/')#, exclude='.bam')
+# aligned BAM files Mayo
+files = synapseutils.syncFromSynapse(syn, 'syn8540821', path = 'BAMs/MayoCBE/')#, exclude='.bam')
+files = synapseutils.syncFromSynapse(syn, 'syn8540820', path = 'BAMs/MayoTCX/')#, exclude='.bam')
+
 # metadata MAYO
 files = synapseutils.syncFromSynapse(syn, 'syn11384571', path = 'metadata/')#, exclude='.bam')
 files = synapseutils.syncFromSynapse(syn, 'syn5223705', path = 'metadata/')#, exclude='.bam')
@@ -31,6 +37,8 @@ files = synapseutils.syncFromSynapse(syn, 'syn3817650', path = 'metadata/')#, ex
 
 # STAR ROSMAP
 #files = synapseutils.syncFromSynapse(syn, 'syn12104384', path = 'ROSMAP/STAR/')#, exclude='.bam')
+# aligned bam files ROSMAP
+files = synapseutils.syncFromSynapse(syn, 'syn8540863', path = 'BAMs/ROSMAP/')#, exclude='.bam')
 # ROSMAP metadata
 files = synapseutils.syncFromSynapse(syn, 'syn3157322', path = 'metadata')#, exclude='.bam')
 files = synapseutils.syncFromSynapse(syn, 'syn11958660', path = 'metadata')#, exclude='.bam')
