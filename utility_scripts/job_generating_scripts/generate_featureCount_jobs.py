33932e824bfc5d1c14e23b040737b29d88aee01d
npklein <niekdeklein@gamail.com>
2019-05-10 16:46:03 +0200

    add extra options to featurecounts

--- utility_scripts/job_generating_scripts/generate_featureCount_jobs.py
+++ utility_scripts/job_generating_scripts/generate_featureCount_jobs.py
@@ -3,24 +3,38 @@ import glob
 import os
 
 parser = argparse.ArgumentParser(description='Generate rMATs files.')
-parser.add_argument('output_directory', help='Outputdir to write results to')
-parser.add_argument('jobs_directory', help='Directory to write jobs to')
 parser.add_argument('cram_base_directory', help='Directory with cramFiles')
+parser.add_argument('jobs_directory', help='Directory to write jobs to')
+parser.add_argument('output_directory', help='Outputdir to write results to')
 parser.add_argument('ref_gtf', help='Reference gtf file location')
+parser.add_argument('feature_type', help='Feature type to count (e.g. exon or transcript)')
+parser.add_argument('--extra_options', help='Extra options to give to featurecounts (e.g. -O --fraction',
+                     nargs='+')
 
 
 args = parser.parse_args()
+extra_options = []
+if args.extra_options:
+    for x in args.extra_options:
+        if len(x) == 1:
+            x = '-'+x
+        else:
+            x = '--'+x
+        extra_options.append(x)
+    extra_options = ' '.join(extra_options)
+else:
+    extra_options = ''
 
-cram_files = glob.glob(args.cram_base_directory+'/**/*cram', recursive=True)
-print('found ',len(cram_files),'cram files')
+cram_files = glob.glob(args.cram_base_directory+'/**/*cram', recursive=True)+glob.glob(args.cram_base_directory+'/**/*bam', recursive=True)
+print('found ',len(cram_files),'cram and bam files')
 
 outdir = args.output_directory
 job_base_dir = args.jobs_directory
 
 if not os.path.exists(outdir):
-    os.mkdir(outdir)
+    os.makedirs(outdir)
 if not os.path.exists(job_base_dir):
-    os.mkdir(job_base_dir)
+    os.makedirs(job_base_dir)
 
 def make_jobs(template):
     x = 0
@@ -42,6 +56,8 @@ def make_jobs(template):
         new_template = new_template.replace('REPLACECRAM', cram)
         new_template = new_template.replace('REPLACEBAM', cram.replace('cram','bam'))
         new_template = new_template.replace('REPLACEGTF',args.ref_gtf)
+        new_template = new_template.replace('REPLACEFEATURETYPE',args.feature_type)
+        new_template = new_template.replace('REPLACEEXTRAOPTIONS',extra_options)
         with open(jobs_dir+'/'+sample+'.sh','w') as out:
             out.write(new_template)
 
@@ -67,14 +83,14 @@ featureCounts \
     -C \ # chimeric fragments (those fragments that have their  two  ends  aligned  to  different  chromosomes)  will  NOT be counted
     -s 0 \ # un-stranded
     -p \ #  fragments (or templates) will be counted insteadof reads.  This option is only applicable for paired-end reads.
-    -t exon \ #  feature type.  Only rows which have the matchedfeature type in the provided GTF annotation file will be in-cluded for read counting
+    -t REPLACEFEATURETYPE \ #  feature type.  Only rows which have the matchedfeature type in the provided GTF annotation file will be in-cluded for read counting
     -g gene_id \ # attribute type used to group features (eg. exons) into meta-features (eg. genes) when GTF annotation is pro-vided. ‘geneid’ by default. This attribute type is usually thegene identifier.
     -O \ # reads (or fragments if-pis specified) will be al-lowed to be assigned to more than one matched meta-feature
     -−fraction \ # each overlapping feature will receive a count of 1/y, where y is the total number of features overlapping with the read
     -a REPLACEGTF \
     -o REPLACEOUT \
-    $TMPDIR/$(basename REPLACEBAM)
-
+    $TMPDIR/$(basename REPLACEBAM) \
+    REPLACEEXTRAOPTIONS
 
 if [ $? -eq 0 ];
 then
