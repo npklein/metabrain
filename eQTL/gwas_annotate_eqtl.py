4f19d8826a18379bd0d85572cb8c419a6327a01e
npklein <niekdeklein@gmail.com>
2019-06-05 10:42:47 +0200

    gwas comparisons

--- eQTL/gwas_annotate_eqtl.py
+++ eQTL/gwas_annotate_eqtl.py
@@ -1,26 +1,41 @@
 import pandas as pd;
 import argparse
-
+import gzip
 parser = argparse.ArgumentParser(description='Compare eQTLgen to MetaBrain')
 parser.add_argument('eQTL_file',
                     help='File with eQTL data')
 parser.add_argument('gwas_file',
                     help='File with gwas data')
-parser.add_argument('target_trait',
-                    help='File with gwas data')
 parser.add_argument('outfile',
                     help='outfile name')
 args = parser.parse_args()
 
-eqtl_data=pd.read_csv(args.eQTL_file, sep='\t');
-gwas_data=pd.read_csv(args.gwas_file,header=None,sep='\t');
-annotated_data=eqtl_data.merge(gwas_data, how='left', left_on=['SNPName'], right_on=[0]);
-gwas_target_trait_num=gwas_data.loc[gwas_data[1].astype(str).str.contains(args.target_trait),:].shape[0];
-gwas_total_num=gwas_data.shape[0];
-annotated_eqtl_target_num=annotated_data.loc[annotated_data[1].astype(str).str.contains(args.target_trait),:].shape[0];
-eqtl_total_num=eqtl_data.shape[0];
-annotated_eqtl_num=annotated_data.loc[annotated_data[1].astype(str).str.contains(args.target_trait),:].shape[0];
+traits = {}
+with open(args.gwas_file) as input_file:
+    header = input_file.readline()
+    for line in input_file:
+        line = line.strip().split('\t')
+        trait = line[7]
+        chr = line[11]
+        pos = line[12]
+        if chr+'_'+pos not in traits:
+            traits[chr+'_'+pos] = set([])
+        traits[chr+'_'+pos].add(trait)
 
-with open(args.outfile,'w') as out:
-    out.write(annotated_data.rename(index=str, columns={0: 'GWAS_SNP', 1: 'GWAS_TRAIT'}).to_csv(sep='\t', index=False,))
+with gzip.open(args.eQTL_file,'rt') as input_file, gzip.open(args.outfile,'wt') as out:
+    header = input_file.readline().strip('\n')
+    split_head = header.split('\t')
+    fdr_index = split_head.index('FDR')
+    out.write(header+'\tGWAS_TRAIT\n')
+    for line in input_file:
+        line = line.strip('\n')
+        if float(line.split('\t')[fdr_index]) >= 0.05:
+            continue
+        chr = line.split('\t')[2]
+        pos = line.split('\t')[3]
+        if chr+'_'+pos in traits:
+            out.write(line+'\t'+';;'.join(traits[chr+'_'+pos])+'\n')
+        else:
+            out.write(line+'\t-\n')
 
+print('written to '+args.outfile)
