1f6ddc7e2dd71a6bd616ed1f4dd2f33a11a795ca
npklein <niekdeklein@gmail.com>
2019-10-21 16:42:50 +0200

    update pca script

--- GeneNetwork/PCA_setup.sh
+++ GeneNetwork/PCA_setup.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
-source /groups/umcg-wijmenga/tmp04/projects/2019-comethylationnetwork/tools/pcapp/mkl/compilers_and_libraries_2019.3.199/linux/mkl/bin/mklvars.sh intel64
-export DYLD_LIBRARY_PATH=/groups/umcg-wijmenga/tmp04/projects/2019-comethylationnetwork/tools/pcapp/mkl/compilers_and_libraries_2019.3.199/linux/mkl/lib/intel64/:/groups/umcg-wijmenga/tmp04/projects/2019-comethylationnetwork/tools/pcapp/clang/llvm-project/build/lib/:$DYLD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/groups/umcg-wijmenga/tmp04/projects/2019-comethylationnetwork/tools/pcapp/clang/llvm-project/build/lib/:$LD_LIBRARY_PATH
+if [ $HOSTNAME=="boxy" ];
+then
+    TMP='tmp03'
+elif [ $HOSTNAME=="calculon" ];
+then
+    TMP='tmp04'
+else
+    echo "HOSTNAME should be calculon or boxy, was $HOSTNAME"
+    exit 1
+fi
+source /groups/umcg-wijmenga/$TMP/projects/2019-comethylationnetwork/tools/pcapp/mkl/compilers_and_libraries_2019.3.199/linux/mkl/bin/mklvars.sh intel64
+echo "adding to DYLD_LIBRARY_PATH:"
+intel64="/groups/umcg-wijmenga/$TMP/projects/2019-comethylationnetwork/tools/pcapp/mkl/compilers_and_libraries_2019.3.199/linux/mkl/lib/intel64/"
+echo $intel64
+llvm="/groups/umcg-wijmenga/$TMP/projects/2019-comethylationnetwork/tools/pcapp/clang/llvm-project/build/lib/"
+echo $llvm
 
+echo "adding to LD_LIBRARY_PATH:"
+echo $llvm
+
+export DYLD_LIBRARY_PATH=$intel64:$llvm:$DYLD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$llvm:$LD_LIBRARY_PATH
+
+echo $DYLD_LIBRARY_PATH
+echo $LD_LIBRARY_PATH
 # After, can run PCA with
-# /groups/umcg-wijmenga/tmp04/projects/2019-comethylationnetwork/tools/pcapp/pcaplusplus/pca.sh
+/groups/umcg-wijmenga/$TMP/projects/2019-comethylationnetwork/tools/pcapp/pcaplusplus/pca
