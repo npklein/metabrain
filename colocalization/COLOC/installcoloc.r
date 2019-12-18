a83f227a2470969b53996033216a30bfdccd6e3f
harmjanwestra <westra.harmjan@outlook.com>
2019-12-18 15:35:55 +0100

    prevent CRAN from being annoying

--- colocalization/COLOC/installcoloc.r
+++ colocalization/COLOC/installcoloc.r
@@ -2,6 +2,6 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
 BiocManager::install("snpStats")
-install.packages("devtools")
+install.packages("devtools", repos="https://lib.ugent.be/CRAN/")
 library(devtools)
 install_github("chr1swallace/coloc")
