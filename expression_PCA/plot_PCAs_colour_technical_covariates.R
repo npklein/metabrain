4114b712ba98336bb60d4f46c173ca038c78a151
npklein <niekdeklein@gmail.com>
2020-02-04 15:52:03 +0100

    add plot with correlation per PC

--- expression_PCA/plot_PCAs_colour_technical_covariates.R
+++ expression_PCA/plot_PCAs_colour_technical_covariates.R
@@ -7,6 +7,8 @@ library(ggrepel)
 library(optparse)
 library(ggExtra)
 library(RColorBrewer)
+library(fastDummies)
+library("psych")
 
 main <- function(){
   options <- get_options()
@@ -14,10 +16,6 @@ main <- function(){
   options <- test()
   PCs <- annotate_PCs(options)
   
-  PCs[which(PCs$age_death=='90+'),]$age_death <- '90'
-  PCs$age_death <- as.numeric(PCs$age_death)
-  PCs$PMI_.in_hours. <- as.numeric(PCs$PMI_.in_hours.)
-  PCs$RIN <- as.numeric(PCs$RIN)
   plot_PCAs(PCs, options)
 }
 
@@ -44,7 +42,7 @@ test <- function(){
   input_dir <- '/Users/NPK/UMCG/git_projects/brain_eQTL/expression_PCA/'
   options$PCA <- paste0(input_dir,"pc1_2.txt")
   options$output <- input_dir
-  options$covariates <- paste0(input_dir,'2020-02-03-freeze2dot1.TMM.Covariates.txt')
+  options$covariates <- paste0(input_dir,'2020-02-04-freeze2dot1.TMM.Covariates.txt')
   return(options)
 }
 
@@ -53,87 +51,74 @@ annotate_PCs <- function(options){
   covariates <- fread(options$covariates)
   
   PCs_with_covariates <- merge(PCs, covariates, by='Sample', all.x=T)
-  #PCs$genotype <- factor(genotype[match(rownames(PCs), genotype$V1),]$V2)
-  return(PCs_with_pheno)
+  return(PCs_with_covariates)
 }
 
-
-make_pca_plot <- function(PCs, PC_x, PC_y, column){
-  if(is.numeric(PCs[[column]])){
-    # add the super small nuber in case y-axis needs to be log scaled, but only for those columns where the max value > 100000 (so that columns with e.g. percentages don't get log scaled)
-    if(max(PCs[!is.na(PCs[column]),][column])> 10000){
-      PCs[!is.na(PCs[column]),][column] <-PCs[!is.na(PCs[column]),][column] +0.00000000000000000000001
-    }
-    PCs[[column]] <- as.numeric(PCs[[column]])
-  }else{
-    colourCount = length(unique(PCs[[column]]))
-    getPalette = colorRampPalette(brewer.pal(8, "Dark2"))
-    dark2ExtendedPallete <- getPalette(colourCount)
-  }
-  
-  p <- ggplot(PCs, aes_string(x=gsub('PC','Comp',PC_x), 
-                         y=gsub('PC','Comp',PC_y), 
-                         colour=column))+
-    geom_point(alpha=0.5)+
-    theme_bw(base_size=20)+
-    xlab(PC_x)+
-    ylab(PC_y)
-  if(is.numeric(PCs[[column]])){
-    p <- p+ scale_color_distiller(palette = "RdPu",na.value="grey80")
-    if(max(PCs[!is.na(PCs[column]),][column])> 10000){
-      p <- p +  guides(colour=guide_legend(paste0('log(',column,')')))
-    }
-  }else{
-    p <- p+scale_colour_manual(values=dark2ExtendedPallete,na.value="grey80")+
-      guides(colour = guide_legend(override.aes = list(size=5)))
-  }
-
-  
-
-  return(p)
-}
 
 plot_PCAs <- function(PCs, options){
   
-  PCs <- PCs[,colSums(is.na(PCs))<nrow(PCs)]
+  PCs <- PCs[,colSums(is.na(PCs))<nrow(PCs), with=FALSE]
+  PCs <- PCs[!is.na(PCs$cohort),]
+  # removing things that are not of interest for correlation (e.g. SampleFull), or that have most samples
+  # in one group so correlation wouldn't give any inf (e.g. per_sequence_quality_scores_R2: pass=6970, fail=38, warn=5)
+  covariates_only <- PCs[,!c('Sample','Comp1','Comp2','Filename_R1', 'Filename_R2','FILTER','pcaFiltered',
+                            'File.type_R2','File.type_R1','FastQC_original_sample','SampleFull',
+                            'Encoding_R1','Encoding_R2','lib_selection','per_sequence_quality_scores_R2',
+                            'per_base_sequence_content_R2','basic_statistics_R2','kmer_content_R2',
+                            'per_sequence_quality_scores_R1','basic_statistics_R1','kmer_content_R1')]
   
-  PCs_subset <- PCs[12:length(colnames(PCs))]
-  PCs_subset <- PCs_subset[!colnames(PCs_subset) %in% c('individualIdentifier.inferred','genotype_id','projid','Flowcell','study','msex',
-                                                        'cohort','rnaseq_id')]
-  columns_in_front <- c('MEDIAN_5PRIME_BIAS_alignment', 'MEDIAN_3PRIME_BIAS_alignment', 'PCT_PF_READS_ALIGNED_alignment','total_reads')
-  for(c in columns_in_front){
-    if(c %in% colnames(PCs_subset)){
-      PCs_subset <- PCs_subset[c(c, setdiff(names(PCs_subset), c))]
-    }
-    
-  }
+ 
+  covariates_only_with_dummies <- dummy_cols(covariates_only)
+  numeric <- which(sapply(covariates_only_with_dummies,is.numeric))
+  covariates_only_with_dummies_numeric <- covariates_only_with_dummies[,numeric,with=F]
+  covariates_only_with_dummies_numeric <- covariates_only_with_dummies_numeric[!is.na(covariates_only_with_dummies_numeric$cohort_GTEx),]
   
-  pdf(paste0(options$output,'figures/all_pca.pdf'),width=12, height=8, onefile = TRUE)
-  # read-length, read-depth, percentage mapped reads 
-  for(column in colnames(PCs_subset)){
-    if(nrow(PCs)/sum(is.na(PCs[[column]])) < 1){
-      print(paste('skip',column))
-      next
-    }
-    if(column == ''){
-      next
-    }
-    name <- paste0('PC1_PC_2_',column)
-    outfile <- paste0(options$output,'/figures/all_pca/',name,'.PCA.png')
-    print(paste(column,'-',outfile))
-    
-    
-    p <- make_pca_plot(PCs, 'PC1', 'PC2', column)
-    print(p)
+  
+  PC1_correlated <- corr.test(PCs$Comp1, covariates_only_with_dummies_numeric,use='complete.obs')
+  PC2_correlated <- corr.test(PCs$Comp2, covariates_only_with_dummies_numeric,use='complete.obs')
+  
+  PC1_r_melt <- melt(PC1_correlated$r)
+  PC1_p_melt <- melt(PC1_correlated$p)
+  PC1 <-  merge(PC1_r_melt, PC1_p_melt, by='Var2')
+  PC1$Var1.x <- NULL
+  PC1$Var1.y <- NULL
+  PC1 <- PC1[!(is.na(PC1$value.x) | is.na(PC1$value.y)),]
+  PC1$PC <- 1
 
-    ggsave(outfile, plot=p, width=12, height=8)
-    #p <- ggMarginal(p, type = "histogram",  groupFill = TRUE)
-    #pdf(outfile, width=12, height=8)
-    #print(p)
-    #dev.off()
-    #print(paste('written to',outfile))
+  PC2_r_melt <- melt(PC2_correlated$r)
+  PC2_p_melt <- melt(PC2_correlated$p)
+  PC2 <-  merge(PC2_r_melt, PC2_p_melt, by='Var2')
+  PC2$Var1.x <- NULL
+  PC2$Var1.y <- NULL
+  PC2 <- PC2[!(is.na(PC2$value.x) | is.na(PC2$value.y)),]
+  PC2$PC <- 2
+  
+  colnames(PC1) <- c('Covariate','R', 'pval','PC')
+  colnames(PC2) <- c('Covariate','R', 'pval','PC')
+  
+  
+  cov_plot <- function(pc,pc_num){
+    ggplot(pc, aes(x=reorder(Covariate,R),y=R,
+                   fill=pval < 0.05,colour='black'))+
+      geom_bar(stat='identity', position='dodge')+
+      theme_bw(12)+ 
+      coord_flip()+
+      scale_fill_manual(values=c('#d8b365','#5ab4ac'))+
+      scale_colour_manual(values=c('black'))+
+      guides(colour = FALSE)+
+      xlab('')+
+      ylab(paste0('Pearson correlation with PC',pc_num))+ 
+      theme(
+        legend.position = c(0.2, 0.98),
+      )
   }
-  dev.off()
+  p_PC1 <- cov_plot(PC1,1)
+  ggsave('figures/correlated_covariates_PC1.pdf',width=6, height=24)
+  p_PC2 <- cov_plot(PC2,2)
+  ggsave('figures/correlated_covariates_PC2.pdf',width=6, height=24)
+    
+  
+  
 }
 
 # runs only when script is run by itself, similar to Python's if __name__ == __main__
