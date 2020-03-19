6554432c24ac9380bf96fe5d163c7089804779a2
mvochteloo <martijn.vochteloo.gmail.com>
2020-03-19 19:56:33 +0100

    interacition matrix merge completed

--- deconvolution/merge_groups/src/main.py
+++ deconvolution/merge_groups/src/main.py
@@ -29,12 +29,13 @@ import os
 import re
 
 # Third party imports.
+import numpy as np
 
 # Local application imports.
-from general.utilities import prepare_output_dir
+from general.utilities import prepare_output_dir, check_file_exists
 from general.local_settings import LocalSettings
 from general.utilities import get_leaf_dir, get_basename
-from general.df_utilities import load_dataframe
+from general.df_utilities import load_dataframe, save_dataframe
 
 
 class Main:
@@ -78,7 +79,7 @@ class Main:
         self.geno_filename = filenames["genotype"]
         self.expr_filename = filenames["expression"]
         self.cov_filename = filenames["covariate"]
-        self.inter_filename = filenames["interaction"]
+        self.inter_regex = filenames["interaction"]
 
     def filter_groups(self):
         g_in_data_indir = glob.glob(os.path.join(self.g_data_indir, 'group_*'))
@@ -96,6 +97,19 @@ class Main:
         print("Starting combining groups.")
         self.print_arguments()
 
+        # Define output filenames.
+        eqtl_outpath = os.path.join(self.outdir, "interaction_table.txt.gz")
+        geno_outpath = os.path.join(self.outdir, "genotype_table.txt.gz")
+        alleles_outpath = os.path.join(self.outdir, "alleles_table.txt.gz")
+        expr_outpath = os.path.join(self.outdir, "expression_table.txt.gz")
+        cov_outpath = os.path.join(self.outdir, "covariate_table.txt.gz")
+        inter_outpath = os.path.join(self.outdir, "interaction_table.txt.gz")
+
+        snp_indices = np.array([], dtype=np.int16)
+        sample_indices = np.array([], dtype=np.int16)
+        inter_df = None
+
+        print("Loading group data.")
         for i, group_id in enumerate(self.group_ids):
             print("\tWorking on: {:10s} [{}/{} "
                   "{:.2f}%]".format(group_id, i + 1, len(self.group_ids),
@@ -103,23 +117,54 @@ class Main:
 
             # Define the directory names.
             data_indir = os.path.join(self.g_data_indir, group_id)
-            inter_indir = os.path.join(self.g_data_indir, group_id)
+            inter_indir = os.path.join(self.g_inter_indir, group_id, 'output')
 
             # Load the group object.
             obj_inpath = os.path.join(data_indir, self.obj_filename + ".pkl")
             with open(obj_inpath, "rb") as f:
                 group_object = pickle.load(f)
-            print(group_object.get_id())
 
-            # Load the interaction file.
-            inter_inpath = ""
-            for path in glob.glob(os.path.join(inter_indir, "*")):
-                if re.match(self.inter_filename, get_basename(path)):
-                    inter_inpath = path
-                    break
-            inter_df = load_dataframe(inpath=inter_inpath, header=0, index_col=0)
-            print(inter_df)
-            exit()
+            # Safe the indices.
+            snp_indices = np.append(snp_indices, group_object.get_snp_indices())
+            sample_indices = np.append(snp_indices, group_object.get_sample_indices())
+
+            if not check_file_exists(inter_outpath) or self.force:
+                # Search for the interaction filename.
+                inter_inpath = None
+                for path in glob.glob(os.path.join(inter_indir, "*")):
+                    if re.match(self.inter_regex, get_basename(path)):
+                        inter_inpath = path
+                        break
+                if inter_inpath is None:
+                    print("Interaction matrix not found.")
+                    exit()
+
+                # Load the interaction file.
+                group_inter_df = load_dataframe(inpath=inter_inpath,
+                                                header=0,
+                                                index_col=0)
+
+                # Merge them.
+                if inter_df is None:
+                    inter_df = group_inter_df
+                else:
+                    inter_df = inter_df.merge(group_inter_df,
+                                              left_index=True,
+                                              right_index=True)
+
+        if not check_file_exists(inter_outpath) or self.force:
+            print("Constructing complete interaction matrix.")
+
+            # Set the indices.
+            inter_df = inter_df.T
+            inter_df["index"] = snp_indices
+            inter_df.sort_values(by=['index'], inplace=True)
+            inter_df.drop(["index"], axis=1, inplace=True)
+            inter_df = inter_df.T
+
+            # Safe.
+            save_dataframe(df=inter_df, outpath=inter_outpath,
+                           index=True, header=True)
 
     def print_arguments(self):
         print("Arguments:")
