0125c9a616d3a0ebcd664f80fa5cc927cda2978a
npklein <niekdeklein@gmail.com>
2020-03-19 16:04:20 +0100

    split up after evd

--- GeneNetwork/scripts_per_step/7_evd_on_correlation.sh
+++ GeneNetwork/scripts_per_step/7_evd_on_correlation.sh
@@ -1,40 +1,28 @@
 #!/bin/bash
 #SBATCH --job-name=evd_on_correlation
 #SBATCH --output=7_evd_on_correlation.out
-#SBATCH --error=7_evd_on_correlationPCA.err
+#SBATCH --error=7_evd_on_correlation.err
 #SBATCH --time=23:59:59
 #SBATCH --cpus-per-task REPLACETHREADS
-#SBATCH --mem 100gb
+#SBATCH --mem REPLACEMEMb
 #SBATCH --nodes 1
-#SBATCH --qos=regular
+#SBATCH --qos=REPLACEQOS
 
-source REPLACEPCASETUP
 set -e
 set -u
+ml Python/3.6.3-foss-2015b
+
+n_PCs=100
+
 output_dir=REPLACEOUTDIR
 mkdir -p $output_dir/7_evd_on_correlation_matrix/
 
-# step 7. Run evd
- on correlation matrix
+# step 7. Run evd on correlation matrix
 cd $output_dir/7_evd_on_correlation_matrix
 
 python REPLACESCRIPTDIR/evd.py REPLACECORMATRIX REPLACECOVCORRECTEDEXPRESSION ./ --svd_solver REPLACESVDSOLVER
 
-n=0
-while read line;
-do
-    if [[ $line == "Cronbach"* ]]; then continue; fi
-    compare=$(echo $line'<'REPLACECRONBACHCUTOFF | bc)
-    if [[ compare -eq 1 ]]; then break; fi
-        ((n=$n+1))
-done < cronbach.txt
-
-cat < eigenvectors.txt | cut -f1-$n > MetaBrain.eigenvectors.cronbach_REPLACECRONBACHCUTOFF.txt
-mv eigenvectors.txt $output_dir/7_PCA_on_correlation_matrix/MetaBrain.eigenvectors.txt
-mv pc-scores.txt $output_dir/7_PCA_on_correlation_matrix/MetaBrain.pc-scores.txt
-gzip $output_dir/7_PCA_on_correlation_matrix/MetaBrain.eigenvectors.txt
-gzip $output_dir/7_PCA_on_correlation_matrix/MetaBrain.pc-scores.txt
-# need unzippd file for next step, so only use zcat
-zcat $output_dir/7_PCA_on_correlation_matrix/MetaBrain.eigenvectors.cronbach_REPLACECRONBACHCUTOFF.txt > $output_dir/7_PCA_on_correlation_matrix/MetaBrain.eigenvectors.cronbach_REPLACECRONBACHCUTOFF.txt.gz
-cd -
-
+mv eigenvectors.txt $output_dir/7_evd_on_correlation_matrix/REPLACENAME.eigenvectors.txt
+mv pc-scores.txt $output_dir/7_evd_on_correlation_matrix/REPLACENAME.pc-scores.txt
+gzip $output_dir/7_evd_on_correlation_matrix/REPLACENAME.eigenvectors.txt
+gzip $output_dir/7_evd_on_correlation_matrix/REPLACENAME.pc-scores.txt
