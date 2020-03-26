a52fa1b66abb4a0788812357a396a249dc5b81b6
npklein <niekdeklein@gmail.com>
2020-03-26 09:11:04 +0100

    add more variables

--- GeneNetwork/scripts_per_step/10_GeneNetwork_predictions.sh
+++ GeneNetwork/scripts_per_step/10_GeneNetwork_predictions.sh
@@ -2,20 +2,37 @@
 #SBATCH --job-name=REPLACENAME
 #SBATCH --output=REPLACENAME.out
 #SBATCH --error=REPLACENAME.err
-#SBATCH --time=5:59:59
-#SBATCH --mem 10gb
+#SBATCH --time=23:59:59
+#SBATCH --mem REPLACEMEMb
 #SBATCH --nodes 1
-#SBATCH --qos=regular
+#SBATCH --qos=REPLACEQOS
 
 ml Java
 
 set -e
 set -u
 mkdir -p $(dirname REPLACEOUT)
-java -jar -Xms30g -Xmx30g REPLACEGENENETWORKDIR/GeneNetworkBackend-1.0.7-SNAPSHOT-jar-with-dependencies.jar \
+
+remove=false
+if [ ! -f REPLACEEIGENVECTORS ]  && [ -f REPLACEEIGENVECTORS.gz ];
+then
+    echo "Eigenvectors are gzipped but this step needs unzipped eigenvectors. Unzipping now"
+    zcat REPLACEEIGENVECTORS.gz > REPLACEEIGENVECTORS
+    remove=true
+fi
+
+java -jar -XmsREPLACEMEM -XmxREPLACEMEM REPLACEGENENETWORKDIR/GeneNetworkBackend-1.0.7-SNAPSHOT-jar-with-dependencies.jar \
   -e REPLACEEIGENVECTORS \
   -p REPLACEIDENTITYMATRIX \
-  -b REPLACEGENENETWORKDIR/PathwayMatrix/REPLACETYPE_genesInPathways_filteredOnEigenVectorGenes.txt \
+  -b REPLACEBACKGROUND \
   -o REPLACEOUT
 
 touch REPLACENAME.finished
+
+
+if [ "$remove" = true ];
+then
+    echo "Removing unzipped eigenvector file"
+    rm REPLACEEIGENVECTORS
+fi
+
