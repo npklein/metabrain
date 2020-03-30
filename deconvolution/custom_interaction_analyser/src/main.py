splitted program for multi node processing + fixed bug in shuffeling + fixed bug in FDR formula

deconvolution/custom_interaction_analyser/src/main.py
@@ -1,7 +1,7 @@
 """
 File:         main.py
 Created:      2020/03/23
-Last Changed: 2020/03/27
+Last Changed: 2020/03/30
 Author:       M.Vochteloo
 
 Copyright (C) 2020 M.Vochteloo
@@ -25,7 +25,6 @@ from __future__ import print_function
 import multiprocessing as mp
 from pathlib import Path
 from datetime import datetime
-from bisect import bisect_left
 import pickle
 import random
 import queue
@@ -35,20 +34,10 @@ import math
 import os
 
 # Third party imports.
-import numpy as np
-import pandas as pd
-import seaborn as sns
-from scipy.stats import uniform
-import matplotlib
-
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-from scipy import stats
 
 # Local application imports.
 from general.local_settings import LocalSettings
 from general.utilities import prepare_output_dir
-from general.df_utilities import save_dataframe
 from .workers import process_worker
 
 
@@ -57,11 +46,12 @@ class Main:
     Main: this class is the main class that calls all other functionality.
     """
 
-    def __init__(self, settings_file, n_eqtls, n_samples, cores):
+    def __init__(self, settings_file, skip_rows, n_eqtls, n_samples, cores):
         """
         Initializer of the class.
 
         :param settings_file: string, the name of the settings file.
+        :param skip_rows: int, the number of rows to skip.
         :param n_eqtls: int, the number of eqtls in the input files.
         :param n_samples: int, the number of samples in the input files.
         :param cores: int, the number of cores to use.
@@ -97,7 +87,7 @@ class Main:
         if cores > host_cores:
             cores = host_cores
 
-        # Set the correct values for chunk size, and cores.
+        # Set the correct values for skip_rows, chunk size, and cores.
         if n_eqtls == 1:
             chunk_size = 1
             cores = 1
@@ -106,10 +96,15 @@ class Main:
             cores = n_eqtls
         else:
             chunk_size = math.ceil(n_eqtls / cores / 2)
+        self.skip_rows = skip_rows
         self.n_cores = cores
         self.n_eqtls = n_eqtls
         self.n_samples = n_samples
         self.chunk_size = chunk_size
+
+        # Create output filepaths.
+        self.pvalues_outfile = os.path.join(self.outdir, settings.get_setting("actual_pvalues_pickle_filename") + "{}.pkl".format(self.skip_rows))
+        self.perm_pvalues_outfile = os.path.join(self.outdir, settings.get_setting("permuted_pvalues_pickle_filename") + "{}.pkl".format(self.skip_rows))
 
     def count_n_eqtls(self):
         print("Finding the number of eQTLs")
@@ -120,6 +115,14 @@ class Main:
             exit()
 
         return geno_nrows
+
+    @staticmethod
+    def get_number_of_rows(inpath):
+        with gzip.open(inpath, 'rb') as f:
+            for i, l in enumerate(f):
+                pass
+        f.close()
+        return i
 
     def count_n_samples(self):
         print("Finding the number of samples")
@@ -132,162 +135,6 @@ class Main:
 
         return geno_ncols
 
-    def start(self):
-        """
-        The method that serves as the pipeline of the whole program.
-        """
-        print("Starting interaction analyser.")
-        self.print_arguments()
-
-        # Start the timer.
-        start_time = time.time()
-        #
-        # # Create queues.
-        # input_manager = mp.Manager()
-        # input_queue = input_manager.Queue()
-        # output_manager = mp.Manager()
-        # output_queue = output_manager.Queue()
-        #
-        # # Fill input queue.
-        # print("Loading input queue.")
-        # self.fill_input_queue(input_queue)
-        #
-        # # Fire off the workers.
-        # processes = []
-        # for worker_id in range(self.n_cores):
-        #     processes.append(mp.Process(target=process_worker,
-        #                                 args=(worker_id,
-        #                                       self.cov_inpath,
-        #                                       self.geno_inpath,
-        #                                       self.expr_inpath,
-        #                                       self.tech_covs,
-        #                                       input_queue,
-        #                                       output_queue,
-        #                                       self.max_end_time,
-        #                                       self.chunk_size,
-        #                                       self.n_permutations)))
-        # for proc in processes:
-        #     proc.start()
-        #
-        # # Empty the output queue.
-        # columns = None
-        # pvalue_data = []
-        # perm_pvalues = []
-        # eqtls_done = 0
-        # total_zscores = self.n_eqtls * (self.n_permutations + 1)
-        # last_print_time = time.time() - 60
-        # print("Waiting for output from workers.")
-        # while eqtls_done != total_zscores:
-        #     now_time = time.time()
-        #     if self.max_end_time <= now_time:
-        #         print("ERROR: max progress time reached")
-        #         exit()
-        #     if now_time - last_print_time >= 60:
-        #         last_print_time = now_time
-        #         print("\tReceived data for {}/{} eQTLs "
-        #               "[{:.2f}%]".format(eqtls_done,
-        #                                  total_zscores,
-        #                                  (100 / total_zscores) * eqtls_done))
-        #
-        #     try:
-        #         if output_queue.empty():
-        #             time.sleep(0.1)
-        #             continue
-        #
-        #         # Get the new output
-        #         (output_type, output) = output_queue.get(True, timeout=1)
-        #         new_index = output[0]
-        #
-        #         # Check the length.
-        #         if columns is not None and len(output) != len(columns):
-        #             print("Length of new ouput on index '{}' does not "
-        #                   "correspond with the remainder of the "
-        #                   "data.".format(new_index))
-        #             exit()
-        #
-        #         # Safe the columns.
-        #         if new_index == -1:
-        #             if columns is None:
-        #                 columns = output
-        #             else:
-        #                 if output != columns:
-        #                     print("Columns are not identical over "
-        #                           "subprocesses.")
-        #         else:
-        #             # Safe the output to the right result.
-        #             eqtls_done += 1
-        #             if output_type == -1:
-        #                 pvalue_data.append(output)
-        #             else:
-        #                 perm_pvalues.extend(output[2:])
-        #
-        #     except queue.Empty:
-        #         time.sleep(1)
-        #         continue
-        # print("\tReceived data for {}/{} eQTLs "
-        #       "[{:.2f}%]".format(eqtls_done,
-        #                          total_zscores,
-        #                          (100 / total_zscores) * eqtls_done))
-        #
-        # # Join the processes.
-        # for proc in processes:
-        #     proc.join()
-        #
-        # # Create a pandas dataframe from the nested list.
-        # pvalue_df = self.create_df(pvalue_data, columns)
-        #
-        # # Pickle the files.
-        # pvalue_df.to_pickle(os.path.join(self.outdir, "pvalue_df.pkl"))
-        # with open(os.path.join(self.outdir, "perm_pvalues.pkl"), "wb") as f:
-        #     pickle.dump(perm_pvalues, f)
-        # f.close()
-
-        # Load the pickled files.
-        pvalue_df = pd.read_pickle(os.path.join(self.outdir, "pvalue_df.pkl"))
-        with open(os.path.join(self.outdir, "perm_pvalues.pkl"), "rb") as f:
-            perm_pvalues = pickle.load(f)
-        f.close()
-
-        # Get the pvalues from the dataframe.
-        pvalues = sorted(pvalue_df.melt()["value"].to_list())
-
-        # Visualise null distribution.
-        self.plot_distributions(pvalues, perm_pvalues, self.outdir)
-
-        # Create a dataframe with adjusted pvalues.
-        adj_pvalue_df = self.create_adj_pvalue_df(pvalue_df,
-                                                  pvalues,
-                                                  perm_pvalues,
-                                                  self.n_permutations)
-
-        # Compare the two pvalue scores.
-        self.compare_pvalue_scores(pvalue_df, adj_pvalue_df, self.outdir)
-
-        # Create a dataframe with z-scores.
-        zscore_df = self.create_zscore_df(adj_pvalue_df)
-
-        # Write the output file.
-        save_dataframe(df=zscore_df,
-                       outpath=os.path.join(self.outdir,
-                                            "interaction_table.txt.gz"),
-                       header=True, index=True)
-
-        # Stop timer.
-        process_time = time.time() - start_time
-
-        # Print the time.
-        minutes, seconds = divmod(process_time, 60)
-        print("Finished in {:.0f} minutes and "
-              "{:.0f} seconds".format(minutes, seconds))
-
-    @staticmethod
-    def get_number_of_rows(inpath):
-        with gzip.open(inpath, 'rb') as f:
-            for i, l in enumerate(f):
-                pass
-        f.close()
-        return i
-
     @staticmethod
     def get_number_of_columns(inpath):
         with gzip.open(inpath, 'rb') as f:
@@ -297,11 +144,121 @@ class Main:
         f.close()
         return ncols
 
+    def start(self):
+        """
+        The method that serves as the pipeline of the whole program.
+        """
+        print("Starting interaction analyser.")
+        self.print_arguments()
+
+        # Start the timer.
+        start_time = time.time()
+
+        # Create queues.
+        input_manager = mp.Manager()
+        input_queue = input_manager.Queue()
+        output_manager = mp.Manager()
+        output_queue = output_manager.Queue()
+
+        # Fill input queue.
+        print("Loading input queue.")
+        njobs = self.fill_input_queue(input_queue)
+        print("Number of jobs to complete: {}".format(njobs))
+
+        # Fire off the workers.
+        processes = []
+        for worker_id in range(self.n_cores):
+            processes.append(mp.Process(target=process_worker,
+                                        args=(worker_id,
+                                              self.cov_inpath,
+                                              self.geno_inpath,
+                                              self.expr_inpath,
+                                              self.tech_covs,
+                                              input_queue,
+                                              output_queue,
+                                              self.max_end_time,
+                                              self.chunk_size,
+                                              self.n_permutations)))
+        for proc in processes:
+            proc.start()
+
+        # Empty the output queue.
+        columns_added = False
+        pvalue_data = []
+        perm_pvalues = []
+        analyses_done = 0
+        total_analyses = self.n_eqtls * (self.n_permutations + 1)
+        print_len = len(str(total_analyses))
+        last_print_time = time.time() - 50
+        print("Waiting for output from workers.")
+        while analyses_done != total_analyses:
+            now_time = time.time()
+            if self.max_end_time <= now_time:
+                print("ERROR: max progress time reached")
+                exit()
+            if now_time - last_print_time >= 60:
+                last_print_time = now_time
+                print("\tReceived data from {:{}d}/{:{}d} analyses "
+                      "[{:.2f}%]".format(analyses_done, print_len,
+                                         total_analyses, print_len,
+                                         (100 / total_analyses) * analyses_done))
+
+            try:
+                if output_queue.empty():
+                    time.sleep(0.1)
+                    continue
+
+                # Get the new output
+                (output_type, output) = output_queue.get(True, timeout=1)
+                new_index = output[0]
+
+                # Safe the columns.
+                if new_index == -1 and not columns_added:
+                    pvalue_data.append(output)
+                    columns_added = True
+                else:
+                    # Safe the output to the right result.
+                    analyses_done += 1
+                    if output_type == -1:
+                        pvalue_data.append(output)
+                    else:
+                        perm_pvalues.extend(output[2:])
+
+            except queue.Empty:
+                time.sleep(1)
+                continue
+        print("\tReceived data from {:{}d}/{:{}d} analyses "
+              "[{:.2f}%]".format(analyses_done, print_len,
+                                 total_analyses, print_len,
+                                 (100 / total_analyses) * analyses_done))
+
+        # Join the processes.
+        for proc in processes:
+            proc.join()
+
+        # Pickle the files.
+        with open(self.pvalues_outfile, "wb") as f:
+            pickle.dump(pvalue_data, f)
+        f.close()
+        with open(self.perm_pvalues_outfile, "wb") as f:
+            pickle.dump(perm_pvalues, f)
+        f.close()
+
+        # Stop timer.
+        process_time = time.time() - start_time
+
+        # Print the time.
+        minutes, seconds = divmod(process_time, 60)
+        print("Finished in {:.0f} minutes and "
+              "{:.0f} seconds".format(minutes, seconds))
+
     def fill_input_queue(self, input_queue):
         # Create a list of start indices.
-        start_indices = [i for i in range(1, self.n_eqtls + 1, self.chunk_size)]
+        start_indices = [i for i in range(self.skip_rows + 1,
+                                          self.n_eqtls + 1,
+                                          self.chunk_size)]
         if self.n_eqtls == 1 and self.chunk_size == 1:
-            start_indices = [1]
+            start_indices = [self.skip_rows + 1]
 
         # Create x random shuffles of the column indices.
         random_shuffles = [self.create_random_shuffle() for _ in
@@ -318,7 +275,7 @@ class Main:
                 input_queue.put((start_index, i, random_shuffle))
                 njobs += 1
 
-        print("Number of jobs to complete: {}".format(njobs))
+        return njobs
 
     def get_column_indices(self):
         return [x for x in range(self.n_samples)]
@@ -327,115 +284,6 @@ class Main:
         column_indices = self.get_column_indices()
         random.shuffle(column_indices)
         return column_indices
-
-    @staticmethod
-    def create_df(data, columns):
-        df = pd.DataFrame(data, columns=columns)
-        df.set_index(df.columns[0], inplace=True)
-        df.sort_index(inplace=True)
-        df.set_index("-", inplace=True)
-        df = df.T
-
-        return df
-
-    @staticmethod
-    def plot_distributions(pvalues, perm_pvalues, outdir):
-        sns.set(rc={'figure.figsize': (12, 9)})
-        sns.set_style("whitegrid")
-        fig, (ax1, ax2) = plt.subplots(1, 2)
-        sns.distplot(perm_pvalues,
-                     fit=uniform,
-                     norm_hist=True,
-                     color='cornflowerblue',
-                     label='null',
-                     kde_kws={'clip': (0.0, 1.0)},
-                     ax=ax1)
-        sns.distplot(pvalues,
-                     norm_hist=True,
-                     color='firebrick',
-                     label='real',
-                     kde_kws={'clip': (0.0, 1.0)},
-                     ax=ax2)
-        for ax, title in zip([ax1, ax2], ["Null", "Alternative"]):
-            ax.text(0.5, 1.02,
-                    '{} Distribution'.format(title),
-                    fontsize=16, weight='bold', ha='center', va='bottom',
-                    transform=ax.transAxes)
-            ax.set_xlabel('p-value',
-                          fontsize=12,
-                          fontweight='bold')
-            ax.set_ylabel('density',
-                          fontsize=12,
-                          fontweight='bold')
-        plt.tight_layout()
-        fig.savefig(os.path.join(outdir, "perm_pvalues_distributions.png"))
-        plt.close()
-
-    @staticmethod
-    def create_adj_pvalue_df(df, pvalues, perm_pvalues, n_perm):
-        pvalues = sorted(pvalues)
-        perm_pvalues = sorted(perm_pvalues)
-        new_df = pd.DataFrame(np.nan, index=df.index, columns=df.columns)
-        for row_index in df.index:
-            for col_index in df.columns:
-                pvalue = df.at[row_index, col_index]
-                rank = bisect_left(pvalues, pvalue) + 1
-                perm_rank = bisect_left(perm_pvalues, pvalue) + 1
-                adj_pvalue = (perm_rank / n_perm) / rank
-                print("P-value: {:.4f}\tRank: {}\tPerm.Rank: {}\t"
-                      "Adj.P-value: {:.4f}".format(pvalue, rank,
-                                               perm_rank, adj_pvalue))
-                new_df.at[row_index, col_index] = adj_pvalue
-
-        return new_df
-
-    def create_zscore_df(self, df):
-        new_df = pd.DataFrame(np.nan, index=df.index, columns=df.columns)
-        for row_index in df.index:
-            for col_index in df.columns:
-                pvalue = df.at[row_index, col_index]
-                zscore = self.get_z_score(pvalue)
-                new_df.at[row_index, col_index] = zscore
-
-        return new_df
-
-    @staticmethod
-    def get_z_score(p_value):
-        if p_value >= 1:
-            return np.nan
-        if p_value <= 0:
-            return np.nan
-
-        return stats.norm.ppf((1 - p_value))
-
-    @staticmethod
-    def compare_pvalue_scores(pvalue_df, adj_pvalue_df, outdir):
-        df = pd.DataFrame({"default": pvalue_df.melt()["value"],
-                           "adjusted": adj_pvalue_df.melt()["value"]})
-        print(df)
-
-        coef, p_value = stats.spearmanr(df["default"], df["adjusted"])
-
-        fig, ax = plt.subplots()
-        sns.set(rc={'figure.figsize': (12, 9)})
-        sns.set_style("ticks")
-        g = sns.regplot(x="default", y="adjusted", data=df,
-                        scatter_kws={'facecolors': '#000000',
-                                     'edgecolor': '#000000',
-                                     'alpha': 0.5},
-                        line_kws={"color": "#D7191C"},
-                        )
-        g.set_title('Z-score regression (Spearman r = {:.2f}, '
-                    'p = {:.2e})'.format(coef, p_value))
-        g.set_ylabel('Adjusted pvalue',
-                     fontsize=8,
-                     fontweight='bold')
-        g.set_xlabel('Default pvalue',
-                     fontsize=8,
-                     fontweight='bold')
-        g.axhline(0, ls='--', color="#D7191C", alpha=0.3, zorder=-1)
-        g.axvline(0, ls='--', color="#D7191C", alpha=0.3, zorder=-1)
-        fig.savefig(os.path.join(outdir, "pvalue_comparison.png"))
 
     def print_arguments(self):
         end_time_string = datetime.fromtimestamp(self.max_end_time).strftime(
@@ -447,6 +295,9 @@ class Main:
         print("  > Output directory: {}".format(self.outdir))
         print("  > Technical covariates: {}".format(self.tech_covs))
         print("  > N permutations: {}".format(self.n_permutations))
+        print("  > Actual P-values output file: {}".format(self.pvalues_outfile))
+        print("  > Permutated P-values output file: {}".format(self.perm_pvalues_outfile))
+        print("  > Skip rows: {}".format(self.skip_rows))
         print("  > N eQTLs: {}".format(self.n_eqtls))
         print("  > N samples: {}".format(self.n_samples))
         print("  > N cores: {}".format(self.n_cores))

deconvolution/custom_interaction_analyser/src/workers.py
@@ -1,7 +1,7 @@
 """
 File:         workers.py
 Created:      2020/03/24
-Last Changed: 2020/03/27
+Last Changed: 2020/03/30
 Author(s):    M.Vochteloo
 
 Copyright (C) 2020 M.Vochteloo
@@ -43,9 +43,6 @@ def process_worker(worker_id, cov_inpath, geno_inpath, expr_inpath, tech_covs,
         "%d-%m-%Y, %H:%M:%S")
     print("Worker: {} started [{}]".format(worker_id, start_time_str))
 
-    # Create time lists.
-    eqtl_process_times = []
-
     # Load the covariate file.
     cov_df = pd.read_csv(cov_inpath,
                          sep="\t",
@@ -54,7 +51,6 @@ def process_worker(worker_id, cov_inpath, geno_inpath, expr_inpath, tech_covs,
 
     tech_cov = cov_df.loc[tech_covs, :]
     cov_df.drop(tech_covs, inplace=True)
-    cov_columns = cov_df.columns
 
     output_q.put((None, [-1] + ["-"] + list(cov_df.index)))
 
@@ -65,7 +61,7 @@ def process_worker(worker_id, cov_inpath, geno_inpath, expr_inpath, tech_covs,
         try:
             if input_q.empty():
                 break
-            (start_index, shuffle_type, column_indices) = input_q.get(True, timeout=1)
+            (start_index, shuffle_type, column_order) = input_q.get(True, timeout=1)
             # print("Worker: {} start_index: {}\tshuffle type: {}\t"
             #       "column_indices: {}".format(worker_id, start_index,
             #                                   shuffle_type, column_indices[:5]))
@@ -90,35 +86,23 @@ def process_worker(worker_id, cov_inpath, geno_inpath, expr_inpath, tech_covs,
             except pandas.io.common.EmptyDataError:
                 break
 
-            # Reorder the genotype dataframe.
-            geno_columns = geno_df.columns
-            geno_df = geno_df.iloc[:, column_indices]
-            geno_df.columns = geno_columns
-
-            # Reorder the covariate dataframe.
-            tmp_cov_df = cov_df.copy()
-            tmp_cov_df = tmp_cov_df.iloc[:, column_indices]
-            tmp_cov_df.columns = cov_columns
-
             # Replace -1 with NaN in the genotype dataframe.
             geno_df.replace(-1, np.nan, inplace=True)
 
             # loop over eQTLs.
             for i in range(chunk_size):
-                eqtl_start_time = time.time()
                 # print("Worker: {}, working on eQTL {}/{}".format(worker_id,
                 #                                                  start_index + i,
                 #                                                  (start_index + chunk_size) - 1))
                 # Get the missing genotype indices.
                 indices = np.arange(geno_df.shape[1])
-                sample_mask = indices[~geno_df.iloc[i, :].isnull().values]
-                n = len(sample_mask)
+                eqtl_indices = indices[~geno_df.iloc[i, :].isnull().values]
+                n = len(eqtl_indices)
 
                 # Subset the row and present samples for this eQTL.
-                genotype = geno_df.iloc[i, sample_mask].copy()
-                expression = expr_df.iloc[i, sample_mask].copy()
-                technical_covs = tech_cov.iloc[:, sample_mask].copy()
-                covariates = tmp_cov_df.iloc[:, sample_mask].copy()
+                genotype = geno_df.iloc[i, eqtl_indices].copy()
+                expression = expr_df.iloc[i, eqtl_indices].copy()
+                technical_covs = tech_cov.iloc[:, eqtl_indices].copy()
 
                 # Check if SNP index are identical.
                 if genotype.name != expression.name:
@@ -131,22 +115,39 @@ def process_worker(worker_id, cov_inpath, geno_inpath, expr_inpath, tech_covs,
 
                 # Loop over the covariates.
                 snp_pvalues = []
-                for j in range(covariates.shape[0]):
+                for j in range(cov_df.shape[0]):
+                    cov_name = cov_df.index[j]
+
                     # print("Worker: {}, working on covariate {}/{}".format(
                     #     worker_id,
                     #     j + 1,
                     #     covariates.shape[0]))
-                    # Append to covariate.
-                    cov_name = covariates.index[j]
-                    cov_of_interest = covariates.iloc[j, :]
+
+                    # Calculate the interaction of the covariate of interest.
+                    covariate_all = cov_df.iloc[j, :].copy()
+                    genotype_all = geno_df.iloc[i, :].copy()
+                    interaction_all = covariate_all * genotype_all
+                    interaction_all.name = cov_name
+
+                    # Reorder the series.
+                    interaction_all = interaction_all.reindex(interaction_all.index[column_order])
+
+                    # Remove missing values.
+                    interaction_all = interaction_all.dropna()
+
+                    # Set the identical names as the null matrix.
+                    interaction_all.index = null_matrix.columns
+
+                    # Create the alternative matrix.
                     alt_matrix = null_matrix.copy()
-                    alt_matrix.loc[cov_name, :] = cov_of_interest * genotype
+                    alt_matrix = alt_matrix.append(interaction_all)
 
                     # Create the alternative model.
                     df_alt, rss_alt = create_model(alt_matrix.T, expression)
 
                     # Compare the models.
-                    pvalue = compare_models(rss_null, rss_alt, df_null, df_alt,
+                    pvalue = compare_models(rss_null, rss_alt,
+                                            df_null, df_alt,
                                             n)
 
                     # Safe.
@@ -156,16 +157,8 @@ def process_worker(worker_id, cov_inpath, geno_inpath, expr_inpath, tech_covs,
                 # print("Worker: {} pushing results".format(worker_id))
                 output_q.put((shuffle_type, [start_index + i] + [genotype.name] + snp_pvalues))
 
-                # Safe the time.
-                eqtl_process_time = time.time() - eqtl_start_time
-                eqtl_process_times.append(eqtl_process_time)
-
         except queue.Empty:
             break
-
-    # Print the process times.
-    print("eqtl_process_times <- c({})".format(
-        ', '.join([str(round(x, 2)) for x in eqtl_process_times])))
 
     # Calculate the worker process time.
     end_time = time.time()
@@ -178,37 +171,6 @@ def process_worker(worker_id, cov_inpath, geno_inpath, expr_inpath, tech_covs,
                                               int(run_time_hour),
                                               int(run_time_min),
                                               int(run_time_sec)))
-
-
-def perform_permutations(null_matrix, cov_of_interest, genotype, expression,
-                         cov_name, rss_null, df_null, pvalue, n, n_permutations):
-    p_values = []
-    pvalue_rank = 0
-    for k in range(n_permutations):
-        # Shuffle the genotypes.
-        inter_array = cov_of_interest * genotype
-        index = inter_array.index
-        inter_array = inter_array.sample(frac=1)
-        inter_array.index = index
-
-        # Append to covariate.
-        perm_matrix = null_matrix.copy()
-        perm_matrix.loc[cov_name, :] = inter_array
-
-        # Create the alternative model.
-        df_alt, rss_alt = create_model(perm_matrix.T, expression)
-
-        # Compare the models.
-        perm_pvalue, _ = compare_models(rss_null, rss_alt, df_null, df_alt,
-                                        n)
-        if perm_pvalue < pvalue:
-            pvalue_rank += 1
-        p_values.append(perm_pvalue)
-
-    # Calculate the emperical FDR.
-    adj_pvalue = calc_adj_p_value(pvalue_rank, n_permutations)
-
-    return adj_pvalue
 
 
 def create_model(X, y):
@@ -240,19 +202,4 @@ def calc_f_value(rss1, rss2, df1, df2, n):
 
 
 def get_p_value(f_value, df1, df2, n):
-    p_value = 1 - stats.f.cdf(f_value,
-                              dfn=(df2 - df1),
-                              dfd=(n - df2))
-    if p_value < 1e-16:
-        p_value = 1e-16
-
-    return p_value
-
-
-def calc_adj_p_value(rank, n_permutations):
-    adj_pvalue = (rank + 1) / (n_permutations + 1)
-    if adj_pvalue >= 1:
-        adj_pvalue = 1 - 1e-16
-    elif adj_pvalue < 1e-16:
-        adj_pvalue = 1e-16
-    return adj_pvalue
+    return stats.f.sf(f_value, dfn=(df2 - df1), dfd=(n - df2))