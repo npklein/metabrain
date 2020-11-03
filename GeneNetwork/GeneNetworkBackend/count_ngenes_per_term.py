bb032d6960342cc2616c498decf4444e2bdae867
npklein <niekdeklein@gmail.com>
2020-11-03 02:18:23 +0100

    fix missing db

--- GeneNetwork/GeneNetworkBackend/count_ngenes_per_term.py
+++ GeneNetwork/GeneNetworkBackend/count_ngenes_per_term.py
@@ -2,47 +2,78 @@
 import gzip
 terms = []
 count_term = {}
+term_type = {}
 print('count go')
-with gzip.open('2020-03-29-goa_human.gaf.gz','rt') as input_file:
-    for line in input_file:
-        if line.startswith('!'):
-            continue
-        line = line.strip().split('\t')
-        if line[4] not in count_term:
-            count_term[line[4]] = 0
-            terms.append(line[4])
-        count_term[line[4]] += 1
+with open('term_order.txt','w') as out:
+    out.write('term\torder\n')
+    x = 1
+    with gzip.open('2020-03-29-goa_human.gaf.gz','rt') as input_file:
+        for index, line in enumerate(input_file):
+            if line.startswith('!'):
+                continue
+            line = line.strip().split('\t')
+            type = None
+            if line[8] == 'F':
+                type = 'GO:MF'
+            elif line[8] == 'P':
+                type = 'GO:BP'
+            elif line[8] == 'C':
+                type = 'GO:CC'
+            else:
+                raise RuntimeError(line[8])
+            if line[4] not in count_term:
+                count_term[line[4]] = 0
+                terms.append(line[4])
+                term_type[line[4]] = type
+                out.write(line[4]+'\t'+str(x)+'\n')
+                x += 1
+            count_term[line[4]] += 1
 
-print('count kegg')
-with gzip.open('2020-03-28-c2.cp.kegg.v7.0.entrez.gmt.gz','rt') as input_file:
-    for line in input_file:
-        line = line.split('\t')
-        count_term[line[0]] = len(line)-2
-        terms.append(line[0])
-
-print('count reactome')
-with gzip.open('2020-03-28-Ensembl2Reactome_All_Levels.txt.gz','rt') as input_file:
-    for line in input_file:
-        line = line.strip().split('\t')
-        if line[1] not in count_term:
-            count_term[line[1]] = 0
-            terms.append(line[1])
-        count_term[line[1]] += 1
-
-
-print('count hpo')
-with gzip.open('2020-03-28-HPO-phenotype-to-genes.txt.gz','rt') as input_file:
-    for line in input_file:
-        if line.startswith('#'):
-            continue
-        line = line.strip().split('\t')
-        if line[0] not in count_term:
-            count_term[line[0]] = 0
+    print('count kegg')
+    with gzip.open('2020-03-28-c2.cp.kegg.v7.0.entrez.gmt.gz','rt') as input_file:
+        for index, line in enumerate(input_file):
+            line = line.split('\t')
+            count_term[line[0]] = len(line)-2
             terms.append(line[0])
-        count_term[line[0]] += 1
+            term_type[line[0]] = 'KEGG'
+            out.write(line[0]+'\t'+str(index)+'\n')
+        
+
+    print('count reactome')
+    with gzip.open('2020-03-28-Ensembl2Reactome_All_Levels.txt.gz','rt') as input_file:
+        x = 1
+        for line in input_file:
+            line = line.strip().split('\t')
+            if line[1] not in count_term:
+                count_term[line[1]] = 0
+                terms.append(line[1])
+                term_type[line[1]] = 'REAC'
+                out.write(line[1]+'\t'+str(x)+'\n')
+                x += 1
+            count_term[line[1]] += 1
 
 
-with open('ngenes_per_term.txt','w') as out:
-    out.write('term\tterm_size\n')
-    for term in terms:
-        out.write(term+'\t'+str(count_term[term])+'\n')
+    print('count hpo')
+    with gzip.open('2020-03-28-HPO-phenotype-to-genes.txt.gz','rt') as input_file:
+        x = 1
+        for line in input_file:
+            if line.startswith('#'):
+                continue
+            line = line.strip().split('\t')
+            if line[0] not in count_term:
+                count_term[line[0]] = 0
+                terms.append(line[0])
+                out.write(line[0]+'\t'+str(x)+'\n')
+                term_type[line[0]] = 'HP'
+                x += 1
+            count_term[line[0]] += 1
+
+
+    with open('ngenes_per_term.txt','w') as out:
+        out.write('term_id\tterm_size\tterm_type\n')
+        for term in terms:
+            out.write(term+'\t'+str(count_term[term])+'\t'+term_type[term]+'\n')
+
+
+
+
---
+++ GeneNetwork/coreg_selector/coreg_selector/coreg_selector.py
@@ -0,0 +1,181 @@
+from termcolor import colored
+from scipy.stats import t
+from scipy.stats import norm
+import math
+import re
+import sqlite3
+import argparse
+from beautifultable import BeautifulTable
+from datetime import datetime
+
+##### !! Don't change values below !! ####
+# Number of genes included in the coreg table
+# Used for calculating bonferonni corrected p-value
+n_genes_all = 59056
+n_genes_amygdala = 53147
+n_genes_basal = 58805
+n_genes_cerebellum = 58805
+n_genes_cortex = 58999
+n_genes_hippo = 58052
+n_genes_hypo = 56667
+n_genes_spinal = 57386
+
+# n_tests = (number of genes * number of genes) / 2 because it is symetrical matrix
+# so only half of tests of full matrix
+n_tests = {'all':(n_genes_all*n_genes_all)/2,'amyg':(n_genes_amygdala*n_genes_amygdala)/2,
+                'basal':(n_genes_basal*n_genes_basal)/2,'cerebellum':(n_genes_cerebellum*n_genes_cerebellum)/2,
+                'cortex':(n_genes_cortex*n_genes_cortex)/2,'hippocampus':(n_genes_hippo*n_genes_hippo)/2,
+                'hypothalamus':(n_genes_hypo*n_genes_hypo)/2,'spinal':(n_genes_spinal*n_genes_spinal)/2}
+
+# Number of eigenvectors used to make the coreg table
+# These are used to calculate the z-scores from the correlation
+n_PCs = {'all':1000,'amyg':74, 'basal':200, 'cerebellum':300,
+             'cortex':500, 'hippocampus':100, 'hypothalamus':200,
+             'spinal':200}
+##### !! Don't change values above !! ####
+
+
+
+
+class GetCoreg():
+    def __enter__(self):
+        return self
+
+    def __init__(self, db_location):
+        # Connection the the database
+        self.conn = sqlite3.connect(db_location)
+        self.c = self.conn.cursor()
+
+    def get_coreg(self,gene1, gene2, regions=['all','amyg','basal','cerebellum',
+                                         'cortex','hippocampus','hypothalamus',
+                                         'spinal']):
+        allowed_regions = set(['all','amyg','basal','cerebellum',
+                                         'cortex','hippocampus','hypothalamus',
+                                         'spinal'])
+        for e in regions:
+            if e not in allowed_regions:
+                raise RuntimeError('regions included "'+e+'". Regions can only include: '+','.join(allowed_regions))
+        gene_pair = (shortenENSG(gene1)+'_'+shortenENSG(gene2),)
+        co_regions_names = 'cor_'+',cor_'.join(regions)
+        self.c.execute("SELECT "+co_regions_names+" FROM correlations where gene_pair=?", 
+                    gene_pair)
+
+        row = self.c.fetchone()
+        if not row:
+            gene_pair_reverse = (shortenENSG(gene2)+'_'+shortenENSG(gene1),)
+            self.c.execute("SELECT "+co_regions_names+" FROM correlations where gene_pair=?",
+                      gene_pair_reverse)
+            row = self.c.fetchone()
+            if not row:
+                raise RuntimeError('Gene pair '+gene1+' + '+gene2+' not in database')
+        return(row)
+
+    def __exit__(self, type, value, traceback):
+        self.conn.close()
+
+    def get_coreg_and_zScores(self,gene1, gene2, regions=['all','amyg','basal','cerebellum',
+                                         'cortex','hippocampus','hypothalamus',
+                                         'spinal']):
+        coreg_scores = self.get_coreg(gene1, gene2, regions)
+        pvalues = []
+        pvalues_bonf = []
+        zscores = []
+        for index, region in enumerate(regions):
+            p_val, zScore = correlationToZ(coreg_scores[index], n_PCs[region])
+            pvalues.append(p_val)
+            zscores.append(zScore)
+            bonf_cor = p_val*n_tests[region]
+            if bonf_cor > 1:
+                bonf_cor = 1
+            pvalues_bonf.append(bonf_cor)
+
+        return(coreg_scores, pvalues, pvalues_bonf, zscores, regions)
+
+
+def correlationToZ(correlation, nrSamples):
+    '''Convert correlation to zscores using sample size'''
+    try:
+        t_val = correlation / math.sqrt((1.0 - correlation * correlation) / (nrSamples - 2))
+    except (ValueError, ZeroDivisionError):
+        print(correlation, nrSamples)
+        raise
+    p_value = 0.0
+    z_score = 0.0
+    if t_val < 0.0:
+        # *2 because .cdf is two-tailed, but this is 1-tailed test
+        p_value = t.cdf(t_val, nrSamples-2) * 2
+        if p_value < 2.0e-323:
+            p_value = 2.0e-323
+        z_score = norm.ppf(p_value);
+    else:
+        # *2 because .cdf is two-tailed, but this is 1-tailed test
+        p_value = t.cdf(-t_val, nrSamples-2) *2
+        if p_value < 2.0e-323:
+            p_value = 2.0e-323
+    
+        z_score = -norm.ppf(p_value)
+    return p_value,z_score
+
+def shortenENSG(string):
+    '''Function to replace all leading
+       zeros from a a given string
+       to a number of replaced zeros
+        e.g. 00023404 becomes 323404
+    '''
+    if string.startswith('ENSG'):
+        string = string.replace('ENSG','')
+    nzeros = len(string) - len(string.lstrip('0'))
+    # Regex to remove leading
+    # zeros from a string
+    regex = "^0+(?!$)"
+    # Replaces the matched
+    # value with given string
+    string = str(nzeros) + re.sub(regex, "", string)
+    return(string)
+
+
+def command_line():
+    '''Use command line arguments to print coreg table
+       This function runs if program is run from the command line
+    '''
+    parser = argparse.ArgumentParser(description='Select correlation between two genes.')
+    parser.add_argument('gene1', help='1st gene in pair')
+    parser.add_argument('gene2', help='2nd  gene in pair')
+    parser.add_argument('db_location', help='database location')
+    parser.add_argument('--regions', help='comma separated regions to select',
+                        default='all,amyg,basal,cerebellum,'+
+                                 'cortex,hippocampus,hypothalamus,'+
+                                 'spinal')
+    parser.add_argument('--debug', action='store_true')
+    args = parser.parse_args()
+    if type(args.regions) == str:
+        args.regions = args.regions.split(',')
+    with  GetCoreg(args.db_location) as get_coreg:
+        coreg_scores, pvalues, pvalues_bonf, zscores, regions = get_coreg.get_coreg_and_zScores(args.gene1, args.gene2, args.regions)
+
+    print('Coreg between '+args.gene1 +' and '+args.gene2)
+
+    table = BeautifulTable(precision=332)
+    table.set_style(BeautifulTable.STYLE_COMPACT)
+    for index, region in enumerate(args.regions):
+        pval =  "{:.2e}".format(pvalues[index])
+        pval_bonf  = "{:.2e}".format(pvalues_bonf[index])
+        coreg = round(coreg_scores[index], 3)
+        zscore = str(round(zscores[index], 3))[0:6]
+        if pvalues[index] < 0.05:
+            pval = colored(pval, "green")
+        if pvalues_bonf[index] < 0.05:
+            pval_bonf =  colored(pval_bonf,"green",attrs=["underline"])
+
+        table.rows.append([region, coreg, zscore,
+                          pval, pval_bonf])
+
+    table.columns.header = [colored('region',attrs=['bold']),colored('coreg',attrs=['bold']),colored('z-score',attrs=['bold']),
+                                colored('p-value',attrs=['bold']),colored('p-value bonf corrected',attrs=['bold'])]
+    print(table)
+
+
+
+
+if __name__ == "__main__":
+    command_line()
