893e2089881b3a4cafc1e10003b3ab9f120c06c2
mvochteloo <martijn.vochteloo@gmail.com>
2020-11-16 17:02:42 +0100

    start of new project: decon-optimizer. Code ready up to and including cell type specific subsetting and cohort force normal transformation

---
+++ decon-optimizer/src/data_object.py
@@ -0,0 +1,170 @@
+"""
+File:         data_object.py
+Created:      2020/11/16
+Last Changed:
+Author:       M.Vochteloo
+
+Copyright (C) 2020 M.Vochteloo
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+A copy of the GNU General Public License can be found in the LICENSE file in the
+root directory of this source tree. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+# Standard imports.
+import os
+
+# Third party imports.
+import pandas as pd
+
+# Local application imports.
+
+
+class Data:
+    def __init__(self, eqtl_path, genotype_path, alleles_path, expression_path,
+                 cell_fractions_path, decon_path,  sample_annotation_path,
+                 sample_id, cohort_id, log):
+        # Safe arguments.
+        self.eqtl_path = eqtl_path
+        self.geno_path = genotype_path
+        self.alle_path = alleles_path
+        self.expr_path = expression_path
+        self.frac_path = cell_fractions_path
+        self.deco_path = decon_path
+        self.saan_path = sample_annotation_path
+        self.sample_id = sample_id
+        self.cohort_id = cohort_id
+        self.log = log
+
+        # Set empty variables.
+        self.eqtl_df = None
+        self.geno_df = None
+        self.alle_df = None
+        self.expr_df = None
+        self.frac_df = None
+        self.deco_df = None
+        self.saan_df = None
+
+    def get_eqtl_df(self, skiprows=None, nrows=None):
+        if self.eqtl_df is None:
+            self.eqtl_df = self.load_dataframe(self.eqtl_path,
+                                               header=0,
+                                               index_col=None,
+                                               skiprows=skiprows,
+                                               nrows=nrows)
+
+        return self.eqtl_df
+
+    def get_geno_df(self, skiprows=None, nrows=None):
+        if self.geno_df is None:
+            self.geno_df = self.load_dataframe(self.geno_path,
+                                               header=0,
+                                               index_col=0,
+                                               skiprows=skiprows,
+                                               nrows=nrows)
+
+            return self.geno_df
+
+    def get_alle_df(self, skiprows=None, nrows=None):
+        if self.alle_df is None:
+            self.alle_df = self.load_dataframe(self.alle_path,
+                                               header=0,
+                                               index_col=0,
+                                               skiprows=skiprows,
+                                               nrows=nrows)
+
+            return self.alle_df
+
+    def get_expr_df(self, skiprows=None, nrows=None):
+        if self.expr_df is None:
+            self.expr_df = self.load_dataframe(self.expr_path,
+                                               header=0,
+                                               index_col=0,
+                                               skiprows=skiprows,
+                                               nrows=nrows)
+
+            return self.expr_df
+
+    def get_frac_df(self, skiprows=None, nrows=None):
+        if self.frac_df is None:
+            self.frac_df = self.load_dataframe(self.frac_path,
+                                               header=0,
+                                               index_col=0,
+                                               skiprows=skiprows,
+                                               nrows=nrows)
+
+            return self.frac_df
+
+    def get_deco_df(self, skiprows=None, nrows=None):
+        if self.deco_df is None:
+            self.deco_df = self.load_dataframe(self.deco_path,
+                                               header=0,
+                                               index_col=None,
+                                               skiprows=skiprows,
+                                               nrows=nrows)
+
+            return self.deco_df
+
+    def load_dataframe(self, inpath, header, index_col, sep="\t", low_memory=True,
+                       nrows=None, skiprows=None):
+        df = pd.read_csv(inpath, sep=sep, header=header, index_col=index_col,
+                         low_memory=low_memory, nrows=nrows, skiprows=skiprows)
+        self.log.info("\tLoaded dataframe: {} "
+                      "with shape: {}".format(os.path.basename(inpath),
+                                              df.shape))
+        return df
+
+    def get_cohort_to_sample_dict(self):
+        sample_cohort_dict = self.get_sample_annotation_dict(key=self.sample_id,
+                                                             value=self.cohort_id)
+
+        return self.reverse_merge_dict(sample_cohort_dict)
+
+    @staticmethod
+    def reverse_merge_dict(dict):
+        out_dict = {}
+        seen_keys = set()
+        for key, value in dict.items():
+            if key in seen_keys:
+                print("Key {} has muiltiple values.".format(key))
+            seen_keys.add(key)
+
+            if value in out_dict.keys():
+                keys = out_dict[value]
+                keys.append(key)
+                out_dict[value] = keys
+            else:
+                out_dict[value] = [key]
+
+        return out_dict
+
+    def get_sample_annotation_dict(self, key, value):
+        if self.saan_df is None:
+            self.saan_df = self.load_dataframe(self.saan_path,
+                                               header=0,
+                                               index_col=None,
+                                               low_memory=False)
+
+        return dict(zip(self.saan_df.loc[:, key], self.saan_df.loc[:, value]))
+
+    def print_arguments(self):
+        self.log.info("Data Arguments:")
+        self.log.info("  > eQTL input path: {}".format(self.eqtl_path))
+        self.log.info("  > Genotype input path: {}".format(self.geno_path))
+        self.log.info("  > Alleles input path: {}".format(self.alle_path))
+        self.log.info("  > Expression input path: {}".format(self.expr_path))
+        self.log.info("  > Cell fractions input path: {}".format(self.frac_path))
+        self.log.info("  > Decon input path: {}".format(self.deco_path))
+        self.log.info("  > Sample annotation path: {}".format(self.saan_path))
+        self.log.info("  > Sample ID: {}".format(self.sample_id))
+        self.log.info("  > Cohort ID: {}".format(self.cohort_id))
+        self.log.info("")
---
+++ decon-optimizer/src/main.py
@@ -0,0 +1,128 @@
+"""
+File:         main.py
+Created:      2020/11/16
+Last Changed:
+Author:       M.Vochteloo
+
+Copyright (C) 2020 M.Vochteloo
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+A copy of the GNU General Public License can be found in the LICENSE file in the
+root directory of this source tree. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+# Standard imports.
+from __future__ import print_function
+from pathlib import Path
+import os
+
+# Third party imports.
+
+# Local application imports.
+from .logger import Logger
+from .data_object import Data
+from .cell_type_object import CellType
+
+
+class Main:
+    def __init__(self, eqtl_path, genotype_path, alleles_path, expression_path,
+                 cell_fractions_path, decon_path, sample_annotation_path,
+                 sample_id, cohort_id, alpha, outdir, extensions,
+                 clear_log):
+        # Safe arguments.
+        self.alpha = alpha
+        self.extensions = extensions
+
+        # Define the current directory.
+        current_dir = str(Path(__file__).parent.parent)
+
+        # Prepare an output directory.
+        self.outdir = os.path.join(current_dir, outdir)
+        if not os.path.exists(self.outdir):
+            os.makedirs(self.outdir)
+
+        # Initialize logger.
+        logger = Logger(outdir=self.outdir, clear_log=clear_log)
+        self.log = logger.get_logger()
+
+        # Initialize data object.
+        self.data = Data(eqtl_path=eqtl_path,
+                         genotype_path=genotype_path,
+                         alleles_path=alleles_path,
+                         expression_path=expression_path,
+                         cell_fractions_path=cell_fractions_path,
+                         decon_path=decon_path,
+                         sample_annotation_path=sample_annotation_path,
+                         sample_id=sample_id,
+                         cohort_id=cohort_id,
+                         log=self.log)
+        self.data.print_arguments()
+
+    def start(self):
+        self.log.info("Starting program.")
+        self.print_arguments()
+
+        self.log.info("Loading data")
+        eqtl_df = self.data.get_eqtl_df()
+        decon_df = self.data.get_deco_df()
+        cell_types = [x for x in decon_df.columns if x not in ["SNPName", "ProbeName"]]
+        self.log.info("\tCell types: {}".format(cell_types))
+
+        eqtl_decon_df = eqtl_df.merge(decon_df,
+                                      left_on=["SNPName", "ProbeName"],
+                                      right_on=["SNPName", "ProbeName"])
+        del eqtl_df, decon_df
+
+        cf_df = self.data.get_frac_df()
+
+        self.log.info("Filtering cell type mediated eQTLs")
+        eqtl_signif_decon_df = eqtl_decon_df.loc[eqtl_decon_df[cell_types].min(axis=1) < self.alpha, :].copy()
+        eqtl_signif_decon_df.reset_index(drop=True, inplace=True)
+        print(eqtl_signif_decon_df)
+
+        self.log.info("Loading genotype / expression data of cell type mediated "
+                      "eQTLs")
+        skiprows = [x+1 for x in eqtl_decon_df.loc[eqtl_decon_df[cell_types].min(axis=1) >= self.alpha, :].index]
+        geno_df = self.data.get_geno_df(skiprows=skiprows, nrows=max(eqtl_signif_decon_df.index))
+        alleles_df = self.data.get_alle_df(skiprows=skiprows, nrows=max(eqtl_signif_decon_df.index))
+        expr_df = self.data.get_expr_df(skiprows=skiprows, nrows=max(eqtl_signif_decon_df.index))
+        cohort_sample_dict = self.data.get_cohort_to_sample_dict()
+
+        self.log.info("Optimizing per cell type")
+        for cell_type in cell_types:
+            # Subset the cell type.
+            ct_mediated_eqtls = eqtl_signif_decon_df.loc[eqtl_signif_decon_df[cell_type] < self.alpha, :].copy()
+            ct_fractions = cf_df.loc[:, [cell_type in x for x in cf_df.columns]].copy()
+            self.log.info("\tCell {} has {} cell type mediated eQTLs (FDR < {})".format(cell_type, ct_mediated_eqtls.shape[0], self.alpha))
+
+            # Create the cell type object.
+            self.log.info("\tCreating cell type object")
+            cell_type = CellType(cell_type=cell_type,
+                                 eqtl_df=ct_mediated_eqtls,
+                                 geno_df=geno_df.iloc[ct_mediated_eqtls.index, :].copy(),
+                                 alleles_df=alleles_df.iloc[ct_mediated_eqtls.index, :].copy(),
+                                 expr_df=expr_df.iloc[ct_mediated_eqtls.index, :].copy(),
+                                 ct_fractions=ct_fractions,
+                                 cohort_sample_dict=cohort_sample_dict,
+                                 log=self.log)
+            exit()
+
+    def print_arguments(self):
+        self.log.info("Arguments:")
+        self.log.info("  > Alpha: {}".format(self.alpha))
+        self.log.info("  > Extensions: {}".format(self.extensions))
+        self.log.info("  > Output directory: {}".format(self.outdir))
+        self.log.info("")
+
+
+
+
