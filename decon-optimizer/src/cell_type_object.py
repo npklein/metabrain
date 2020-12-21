improved decon-optimizer, optimizer implemented + correct ll calculation

decon-optimizer/src/cell_type_object.py
@@ -1,7 +1,7 @@
 """
 File:         cell_type_object.py
 Created:      2020/11/16
-Last Changed: 2020/11/18
+Last Changed: 2020/12/21
 Author:       M.Vochteloo
 
 Copyright (C) 2020 M.Vochteloo
@@ -21,6 +21,7 @@ root directory of this source tree. If not, see <https://www.gnu.org/licenses/>.
 """
 
 # Standard imports.
+import pickle
 import time
 
 # Third party imports.
@@ -29,6 +30,8 @@ import pandas as pd
 
 # Local application imports.
 from .mle import MaximumLiklihoodEstimator
+from .utilities import force_normal_series
+from scipy.optimize import fmin
 
 
 class CellType:
@@ -53,6 +56,7 @@ class CellType:
         #self.cf_s = self.get_combined_matrix(df="cf_df", type="normal")
         self.cf_s = self.get_combined_matrix(df="cf_df")
         print(self.cf_s)
+        self.norm_cf_s = force_normal_series(self.cf_s, as_series=True)
 
         # Create MLE objects.
         self.mle_objects = self.create_mle_objects()
@@ -143,8 +147,8 @@ class CellType:
         mle_objects = {}
         for index, row in self.eqtl_df.iterrows():
             key = row["SNPName"] + "_" + row["ProbeName"]
-            mle_objects[key] = MaximumLiklihoodEstimator(genotype=self.geno_df.iloc[[index], :].copy(),
-                                                         cell_fractions=self.cf_s,
+            mle_objects[key] = MaximumLiklihoodEstimator(genotype=self.geno_df.iloc[index, :].copy(),
+                                                         cell_fractions=self.norm_cf_s,
                                                          expression=self.expr_df.iloc[index, :].copy(),
                                                          log=self.log)
 
@@ -156,30 +160,21 @@ class CellType:
         start_time = int(time.time())
 
         original_cf = self.cf_s.loc[sample]
-        all_data = []
-        indices = []
-        columns = list(np.arange(0, 1.025, 0.025))
-        #columns = list(np.arange(-8, 8, 0.1))
+        data = []
         for i, (eqtl_name, mle_object) in enumerate(self.mle_objects.items()):
             if (i % 25 == 0) or (i == (self.n - 1)):
                 self.log.info("\t\tProcessing eQTL {} / {} [{:.2f}%]".format(i, self.n - 1, (100/(self.n - 1)) * i))
-            row_data = []
-            for cell_fraction in columns:
-                #original_cf, new_cf_s = self.calculate_new_normalized_cf_s(sample, cell_fraction)
-                new_cf_s = pd.Series([cell_fraction], index=[sample])
-                row_data.append(mle_object.get_maximum_log_likelihood(cell_fractions=new_cf_s))
+            if mle_object.contains_sample(sample):
+                optimum = fmin(mle_object.likelihood_optimization,
+                               x0=np.array([original_cf]),
+                               args=(sample, ),
+                               disp=0)
+                print(i, optimum)
+                data.append(optimum[0])
 
-            all_data.append(row_data)
-            indices.append(eqtl_name)
-
-        results = pd.DataFrame(all_data, index=indices, columns=columns)
-        results.dropna(axis=0, inplace=True)
+        results = pd.Series(data)
         print(results)
-        cell_frac_results = results.sum(axis=0)
-        print(cell_frac_results)
-
-        self.log.info("\tReal fraction: {:.4f}\tmll fraction: {:.4f}".format(original_cf,
-                                                                             cell_frac_results.idxmin()))
+        self.log.info("\tReal fraction: {:.4f}\tmml fraction: mean = {:.4f}  median = {:.4f}".format(original_cf, results.mean(), results.median()))
 
         run_time_min, run_time_sec = divmod(int(time.time()) - start_time, 60)
         self.log.info("\tFinished in {} minute(s) and "

decon-optimizer/src/mle.py
@@ -1,7 +1,7 @@
 """
 File:         mle.py
 Created:      2020/11/17
-Last Changed: 2020/11/18
+Last Changed: 2020/12/21
 Author:       M.Vochteloo
 
 Copyright (C) 2020 M.Vochteloo
@@ -24,6 +24,7 @@ root directory of this source tree. If not, see <https://www.gnu.org/licenses/>.
 
 # Third party imports.
 import numpy as np
+import pandas as pd
 from scipy import stats
 import statsmodels.api as sm
 
@@ -36,12 +37,15 @@ class MaximumLiklihoodEstimator:
         self.X, self.y = self.construct_model_matrix(genotype,
                                                      cell_fractions,
                                                      expression)
-        self.samples = list(self.X.index)
         self.log = log
 
-    def construct_model_matrix(self, genotype, cell_fractions, y):
-        # Construct the X matrix.
-        X = genotype.T.merge(cell_fractions.to_frame(), left_index=True, right_index=True)
+        # Calculate distribution properties.
+        self.mu = self.y.mean()
+        self.sd = self.y.std()
+
+    @staticmethod
+    def construct_model_matrix(genotype, cell_fractions, y):
+        X = pd.concat([genotype, cell_fractions], axis=1)
         X.columns = ["genotype", "cell_fractions"]
         X.insert(loc=0, column='intercept', value=1)
         X["genotype_x_cell_fractions"] = X["genotype"] * X["cell_fractions"]
@@ -54,18 +58,31 @@ class MaximumLiklihoodEstimator:
     def get_n(self):
         return self.X.shape[0]
 
-    def get_maximum_log_likelihood(self, cell_fractions=None):
+    def contains_sample(self, sample):
+        return sample in self.X.index
+
+    def likelihood_optimization(self, value=None, sample=None):
+        return -1 * self.get_log_likelihood(sample=sample, value=value)
+
+    def get_log_likelihood(self, sample=None, value=None):
         X = self.X
-        if cell_fractions is not None:
-            X = self.change_sample_cell_fraction(cell_fractions=cell_fractions)
+        if sample is not None and value is not None:
+            X = self.change_sample_cell_fraction(sample=sample,
+                                                 value=value)
 
         y_hat = self.create_model(X, self.y)
-        mll = self.calculate_maximum_log_likelihood(self.y, y_hat)
+        ll = self.calculate_log_likelihood(y_hat)
 
-        return mll
+        return ll
+
+    def change_sample_cell_fraction(self, sample, value):
+        X = self.X.copy()
+        X.loc[sample, "cell_fractions"] = value
+        X.loc[sample, "genotype_x_cell_fractions"] = X.loc[sample, "genotype"] * value
+
+        return X
 
     def create_model(self, X, y):
-        # Perform the Ordinary least squares fit.
         ols = sm.OLS(y.values, X)
         try:
             ols_result = ols.fit()
@@ -74,28 +91,9 @@ class MaximumLiklihoodEstimator:
             self.log.error("\tError in OLS: {}".format(e))
             return np.nan
 
-    @staticmethod
-    def calculate_maximum_log_likelihood(y, y_hat):
-        mu = y.mean()
-        sd = y.std()
+    def calculate_log_likelihood(self, y_hat):
+        return -np.sum(stats.norm.logpdf(y_hat, self.mu, self.sd))
 
-        return np.log(stats.norm.pdf(y_hat, mu, sd)).sum()
-
-    def change_sample_cell_fraction(self, cell_fractions):
-        # Remove samples that have genotype of '-1'.
-        keep = []
-        for sample in cell_fractions.index:
-            if sample in self.X.index:
-                keep.append(sample)
-        cell_fractions = cell_fractions.loc[keep]
-
-        # Replace the cell fractions values for all samples that are part of
-        # the cohort of which we want to change 1 value.
-        X = self.X.copy()
-        X.loc[cell_fractions.index, "cell_fractions"] = cell_fractions
-        X.loc[cell_fractions.index, "genotype_x_cell_fractions"] = X.loc[cell_fractions.index, "genotype"] * cell_fractions
-
-        return X
 
 
 