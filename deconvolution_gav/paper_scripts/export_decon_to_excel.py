4ef6544dd4bc089bef9bb2a42ade7f821d156df1
mvochteloo <martijn.vochteloo@gmail.com>
2021-02-26 18:37:53 +0100

    removed old files + combined scripts to merge and fill mr / decon table + moved scripts that are no longer used to trash folder + fixed bug in alleles coding

--- deconvolution_gav/paper_scripts/export_decon_to_excel.py
+++ deconvolution_gav/paper_scripts/export_decon_to_excel.py
@@ -56,27 +56,37 @@ __description__ = "{} is a program developed and maintained by {}. " \
 class main():
     def __init__(self):
         self.data = {"cortex": {"decon": "../2020-11-20-decon-QTL/cis/cortex/decon_out/deconvolutionResults.csv",
-                                "eqtl": "../matrix_preparation/cortex_eur_cis/combine_eqtlprobes/eQTLprobes_combined.txt.gz"},
+                                "eqtl": "../matrix_preparation/cortex_eur_cis/combine_eqtlprobes/eQTLprobes_combined.txt.gz",
+                                "alleles": "../matrix_preparation/cortex_eur_cis/create_matrices/genotype_alleles.txt.gz"},
                      "cerebellum": {"decon": "../2020-11-20-decon-QTL/cis/cerebellum/decon_out/deconvolutionResults.csv",
-                                    "eqtl": "../matrix_preparation/cerebellum_eur_cis/combine_eqtlprobes/eQTLprobes_combined.txt.gz"}}
+                                    "eqtl": "../matrix_preparation/cerebellum_eur_cis/combine_eqtlprobes/eQTLprobes_combined.txt.gz",
+                                    "alleles": "../matrix_preparation/cerebellum_eur_cis/create_matrices/genotype_alleles.txt.gz"}}
+        self.gene_info_path = "/groups/umcg-biogen/tmp01/annotation/gencode.v32.primary_assembly.annotation.collapsedGenes.ProbeAnnotation.TSS.txt.gz"
 
         # Set variables.
         outdir = os.path.join(str(Path(__file__).parent.parent), 'export_decon_to_excel')
         if not os.path.exists(outdir):
             os.makedirs(outdir)
 
-        self.outfile = os.path.join(outdir, "Supplementary Table 2  - deconvoluted eQTLs.xlsx")
+        self.outfile = os.path.join(outdir, "Supplementary Table 7 - Deconvoluted eQTLs.xlsx")
 
     def start(self):
         self.print_arguments()
+
+        print("Load gene info.")
+        gene_info_df = self.load_file(self.gene_info_path)
+        gene_dict = dict(zip(gene_info_df["ArrayAddress"], gene_info_df["Symbol"]))
+        del gene_info_df
 
         with pd.ExcelWriter(self.outfile) as writer:
             for tissue, data in self.data.items():
                 decon_path = data["decon"]
                 eqtl_path = data["eqtl"]
+                alleles_path = data["alleles"]
                 print("Tissue: {}".format(tissue))
                 print("  > Decon file: {}".format(decon_path))
                 print("  > eQTL file: {}".format(eqtl_path))
+                print("  > Alleles file: {}".format(alleles_path))
 
                 # Load decon data.
                 decon_df = self.load_file(decon_path)
@@ -87,9 +97,10 @@ class main():
                 for index in decon_df.index:
                     probe_names.append(index.split("_")[0])
                     snp_names.append("_".join(index.split("_")[1:]))
-                decon_df["ProbeName"] = probe_names
-                decon_df["SNPName"] = snp_names
+                decon_df["Gene"] = probe_names
+                decon_df["SNP"] = snp_names
 
+                decon_columns = []
                 new_columns = []
                 for col in decon_df.columns:
                     parts = re.split('_|:', col)
@@ -103,17 +114,24 @@ class main():
                         new_columns.append(col)
                 decon_df.columns = new_columns
 
+                # Load alleles data.
+                alleles_data = self.load_file(alleles_path, index_col=None)
+                alleles_data.columns = ["SNP", "Alleles", "MinorAllele"]
+                snp_to_alles_dict = dict(zip(alleles_data["SNP"], alleles_data["Alleles"]))
+                del alleles_data
+
                 # Load eQTL data.
                 eqtl_df = self.load_file(eqtl_path, index_col=None)
-                eqtl_df = eqtl_df.loc[:, ["ProbeName", "SNPName", "SNPType"]]
-
-                # Pre-process eQTL.
-                eqtl_df["AlleleAssessed"] = eqtl_df["SNPType"].str.split("/", n=1, expand=True)[1]
+                eqtl_df = eqtl_df.loc[:, ["ProbeName", "SNPName"]]
+                eqtl_df["HGNCName"] = eqtl_df["ProbeName"].map(gene_dict)
+                eqtl_df = eqtl_df.loc[:, ["ProbeName", "HGNCName", "SNPName"]]
+                eqtl_df.columns = ["Gene", "Gene symbol", "SNP"]
+                eqtl_df["Alleles"] = eqtl_df["SNP"].map(snp_to_alles_dict)
+                eqtl_df["Allele Assessed"] = eqtl_df["Alleles"].str.split("/", n=1, expand=True)[1]
 
                 # Merge.
-                df = eqtl_df.merge(decon_df,
-                                   left_on=["ProbeName", "SNPName"],
-                                   right_on=["ProbeName", "SNPName"])
+                df = eqtl_df.merge(decon_df, on=["Gene", "SNP"])
+                print(df)
 
                 # Save
                 df.to_excel(writer, sheet_name=tissue, na_rep="NA", index=False)
