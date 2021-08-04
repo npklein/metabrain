b8d569511d2d93983a127c85a6faf92971d02ebb
mvochteloo <martijn.vochteloo@gmail.com>
2021-08-04 17:23:56 +0200

    added functionality to plot eQTl effect replication to visualise_opposite_effects.py

--- 2021-deKleinEtAl/6-deconvolution/deconvolution/decon-eqtl_scripts/visualise_opposite_effects.py
+++ 2021-deKleinEtAl/6-deconvolution/deconvolution/decon-eqtl_scripts/visualise_opposite_effects.py
@@ -57,6 +57,8 @@ __description__ = "{} is a program developed and maintained by {}. " \
 Syntax:
 ./visualise_opposite_effects.py -dq /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-05-26-eqtls-rsidfix-popfix/cis/2020-05-26-Cortex-EUR/Iteration1/eQTLProbesFDR0.05-ProbeLevel.txt.gz -dg /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-10-12-deconvolution/deconvolution/matrix_preparation/CortexEUR-cis/create_matrices/genotype_table.txt.gz -de /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-10-12-deconvolution/deconvolution/preprocess_scripts/select_and_reorder_matrix/CortexEUR-cis/MetaBrain.allCohorts.2020-02-16.TMM.freeze2dot1.SampleSelection.SampleSelection.ProbesWithZeroVarianceRemoved.Log2Transformed.CovariatesRemovedOLS.ExpAdded.txt -dcc /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-10-12-deconvolution/deconvolution/matrix_preparation/CortexEUR-cis/perform_deconvolution/deconvolution_table.txt.gz -dn EUR -rq /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-05-26-eqtls-rsidfix-popfix/cis/2020-05-26-Cortex-AFR/replicateCortex-EUR/eQTLProbesFDR0.05-ProbeLevel.txt.gz -rg /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-10-12-deconvolution/deconvolution/preprocess_scripts/decon_eqtl_replication_select_and_harmonize/CortexAFR-cis-Replication-EUR/genotype_table.txt -re /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-10-12-deconvolution/deconvolution/preprocess_scripts/decon_eqtl_replication_select_and_harmonize/CortexAFR-cis-Replication-EUR/MetaBrain.allCohorts.2020-02-16.TMM.freeze2dot1.SampleSelection.SampleSelection.ProbesWithZeroVarianceRemoved.Log2Transformed.CovariatesRemovedOLS.ExpAdded.txt -rcc /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-10-12-deconvolution/deconvolution/matrix_preparation/CortexAFR-cis-Replication-EUR/perform_deconvolution/deconvolution_table.txt.gz -rn AFR -i /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-10-12-deconvolution/deconvolution/decon-eqtl_scripts/decon_eqtl_replication_plot/data/replication_opposite_efects_table.txt.gz
 
+./visualise_opposite_effects.py -dq /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-05-26-eqtls-rsidfix-popfix/cis/2020-05-26-Cortex-EUR/Iteration1/eQTLProbesFDR0.05-ProbeLevel.txt.gz -dg /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-10-12-deconvolution/deconvolution/matrix_preparation/CortexEUR-cis/create_matrices/genotype_table.txt.gz -de /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-10-12-deconvolution/deconvolution/matrix_preparation/CortexEUR-cis/create_matrices/expression_table.txt.gz -dcc /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-10-12-deconvolution/deconvolution/matrix_preparation/CortexEUR-cis/perform_deconvolution/deconvolution_table.txt.gz -dn EUR -rq /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-05-26-eqtls-rsidfix-popfix/cis/2020-05-26-Cortex-AFR/replicateCortex-EUR/eQTLProbesFDR0.05-ProbeLevel.txt.gz -rg /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-10-12-deconvolution/deconvolution/preprocess_scripts/decon_eqtl_replication_select_and_harmonize/CortexAFR-cis-Replication-EUR/genotype_table.txt -re /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-10-12-deconvolution/deconvolution/matrix_preparation/CortexAFR-cis-Replication-EUR/create_matrices/expression_table.txt.gz -rcc /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-10-12-deconvolution/deconvolution/matrix_preparation/CortexAFR-cis-Replication-EUR/perform_deconvolution/deconvolution_table.txt.gz -rn AFR -i /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-10-12-deconvolution/deconvolution/decon-eqtl_scripts/decon_eqtl_replication_plot/data/replication_opposite_efects_table.txt.gz
+
 ./visualise_opposite_effects.py -dq /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-05-26-eqtls-rsidfix-popfix/cis/2020-05-26-Cortex-EUR/Iteration1/eQTLProbesFDR0.05-ProbeLevel.txt.gz -dg /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-10-12-deconvolution/deconvolution/matrix_preparation/CortexEUR-cis/create_matrices/genotype_table.txt.gz -de /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-10-12-deconvolution/deconvolution/matrix_preparation/CortexEUR-cis/create_matrices/expression_table.txt.gz -dcc /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-10-12-deconvolution/deconvolution/matrix_preparation/CortexEUR-cis/perform_deconvolution/deconvolution_table.txt.gz -dn EUR -rq /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-05-26-eqtls-rsidfix-popfix/cis/2020-05-26-Cortex-AFR/replicateCortex-EUR/eQTLProbesFDR0.05-ProbeLevel.txt.gz -rg /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-10-12-deconvolution/deconvolution/matrix_preparation/CortexAFR-cis-Replication-EUR/create_matrices/genotype_table.txt.gz -re /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-10-12-deconvolution/deconvolution/matrix_preparation/CortexAFR-cis-Replication-EUR/create_matrices/expression_table.txt.gz -rcc /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-10-12-deconvolution/deconvolution/matrix_preparation/CortexAFR-cis-Replication-EUR/perform_deconvolution/deconvolution_table.txt.gz -rn AFR -i /groups/umcg-biogen/tmp01/output/2019-11-06-FreezeTwoDotOne/2020-10-12-deconvolution/deconvolution/decon-eqtl_scripts/decon_eqtl_replication_plot/data/replication_opposite_efects_table.txt.gz
 """
 
@@ -183,9 +185,11 @@ class main():
         print("Loading interest")
         interest_df = self.load_file(self.interest_path, header=0, index_col=0)
         nrows = max(max(interest_df["discovery index"]), max(interest_df["replication index"])) + 1
+        # nrows = None
 
         print("Loading discovery data")
         d_eqtl_df = self.load_file(self.d_eqtl_path, header=0, index_col=None, nrows=nrows)
+        d_eqtl_df.index = d_eqtl_df["ProbeName"] + "_" + d_eqtl_df["SNPName"]
         d_geno_df = self.load_file(self.d_geno_path, header=0, index_col=0, nrows=nrows)
         # d_allele_df = self.load_file(self.d_allele_path, header=0, index_col=0, nrows=nrows)
         d_expr_df = self.load_file(self.d_expr_path, header=0, index_col=0, nrows=nrows)
@@ -193,6 +197,7 @@ class main():
 
         print("Loading replication data")
         r_eqtl_df = self.load_file(self.r_eqtl_path, header=0, index_col=None, nrows=nrows)
+        r_eqtl_df.index = r_eqtl_df["ProbeName"] + "_" + r_eqtl_df["SNPName"]
         r_geno_df = self.load_file(self.r_geno_path, header=0, index_col=0, nrows=nrows)
         # r_allele_df = self.load_file(self.r_allele_path, header=0, index_col=0, nrows=nrows)
         r_expr_df = self.load_file(self.r_expr_path, header=0, index_col=0, nrows=nrows)
@@ -247,6 +252,66 @@ class main():
             plt.close()
             print("\tSaved: {}".format(outpath))
 
+        # r_expr_df = r_expr_df.groupby(r_expr_df.index).first()
+        # r_expr_df = r_expr_df.loc[r_eqtl_df["ProbeName"], :]
+
+        # if list(d_geno_df.index) != list(d_eqtl_df["SNPName"].values):
+        #     print("Unequal input matrix.")
+        #     exit()
+        # if list(d_expr_df.index) != list(d_eqtl_df["ProbeName"].values):
+        #     print("Unequal input matrix.")
+        #     exit()
+        # if list(r_geno_df.index) != list(r_eqtl_df["SNPName"].values):
+        #     print("Unequal input matrix.")
+        #     exit()
+        # if list(r_expr_df.index) != list(r_eqtl_df["ProbeName"].values):
+        #     print("Unequal input matrix.")
+        #     exit()
+        #
+        # print("Modelling expression ~ genotype")
+        # d_coefs_a = []
+        # d_indices_a = []
+        # replicating_eqtls = set(r_eqtl_df.index)
+        # for i, (index, row) in enumerate(d_eqtl_df.iterrows()):
+        #     if index in replicating_eqtls:
+        #         genotype = d_geno_df.iloc[i, :]
+        #         expression = d_expr_df.iloc[i, :]
+        #         mask = genotype != -1
+        #         coef = np.nan
+        #         if np.std(genotype[mask]) != 0 and np.std(expression[mask]) != 0:
+        #             coef, _ = stats.pearsonr(genotype[mask], expression[mask])
+        #         d_coefs_a.append(coef)
+        #         d_indices_a.append(index)
+        # d_coefs_s = pd.Series(d_coefs_a, index=d_indices_a)
+        # del d_coefs_a, d_indices_a
+        #
+        # r_coefs_a = []
+        # r_indices_a = []
+        # for i, index in enumerate(r_eqtl_df.index):
+        #     genotype = r_geno_df.iloc[i, :]
+        #     expression = r_expr_df.iloc[i, :]
+        #     mask = genotype != -1
+        #     coef = np.nan
+        #     if np.std(genotype[mask]) != 0 and np.std(expression[mask]) != 0:
+        #         coef, _ = stats.pearsonr(genotype[mask], expression[mask])
+        #     r_coefs_a.append(coef)
+        #     r_indices_a.append(index)
+        # r_coefs_s = pd.Series(r_coefs_a, index=r_indices_a)
+        # del r_coefs_a, r_indices_a
+        #
+        # print("Plotting comparison")
+        # plot_df = d_coefs_s.to_frame().merge(r_coefs_s.to_frame(), left_index=True, right_index=True)
+        # plot_df.columns = ["x", "y"]
+        # opposite_effects = set(interest_df["index"].values)
+        # plot_df["hue"] = ["#0072B2" if x in opposite_effects else "#000000" for x in plot_df.index]
+        # print(plot_df)
+        # print(plot_df.loc[plot_df["hue"] == "#0072B2", :])
+        # plot_df.to_csv("visualise_oppositer_effects_plot_df.txt.gz", header=True, index=True, sep="\t", compression="gzip")
+        # self.plot_replication(df=plot_df,
+        #                       xlabel=self.d_name,
+        #                       ylabel=self.r_name,
+        #                       title="expression ~ genotype")
+
     @staticmethod
     def load_file(path, sep="\t", header=0, index_col=None, nrows=None):
         df = pd.read_csv(path, sep=sep, header=header, index_col=index_col,
@@ -259,7 +324,7 @@ class main():
     @staticmethod
     def plot_eqtl(df, palette, ax, title="", xlabel="", ylabel="", annotate=None):
         # Calculate the correlation.
-        coef, _ = stats.spearmanr(df["genotype"], df["expression"])
+        coef, _ = stats.pearsonr(df["genotype"], df["expression"])
 
         # Plot the scatter / box plot.
         sns.regplot(x="genotype", y="expression", data=df,
@@ -321,7 +386,7 @@ class main():
             coef = np.nan
             if len(subset.index) > 1:
                 # Calculate the correlation.
-                coef, _ = stats.spearmanr(df["cell count"], df["expression"])
+                coef, _ = stats.pearsonr(df["cell count"], df["expression"])
 
                 # Plot the scatter / box plot.
                 sns.regplot(x="cell count", y="expression", data=subset,
@@ -361,6 +426,86 @@ class main():
         ax.set_xlabel(xlabel,
                       fontsize=14,
                       fontweight='bold')
+
+    def plot_replication(self, df, x="x", y="y", hue="hue", xlabel="",
+                         ylabel="", title=""):
+        if df.shape[0] <= 2:
+            return
+
+        sns.set_style("ticks")
+        fig, ax = plt.subplots(figsize=(12, 12))
+        sns.set(color_codes=True)
+
+        sns.despine(fig=fig, ax=ax)
+
+        lower_quadrant = df.loc[(df[x] < 0) & (df[y] < 0), :]
+        upper_quadrant = df.loc[(df[x] > 0) & (df[y] > 0), :]
+        concordance = (100 / df.shape[0]) * (lower_quadrant.shape[0] + upper_quadrant.shape[0])
+
+        coef, _ = stats.pearsonr(df[y], df[x])
+
+        sns.regplot(x=x, y=y, data=df, ci=None,
+                    scatter_kws={'facecolors': df["hue"],
+                                 'linewidth': 0,
+                                 'alpha': 0.75},
+                    line_kws={"color": "#0072B2",
+                              'linewidth': 5},
+                    ax=ax)
+        sns.scatterplot(x=x, y=y, data=df.loc[df["hue"] == "#0072B2", :],
+                        hue="hue", palette={"#000000":"#000000", "#0072B2":"#0072B2"},
+                        legend=False,
+                        ax=ax)
+
+        ax.annotate(
+            'N = {}'.format(df.loc[df["hue"] == "#000000", :].shape[0]),
+            xy=(0.03, 0.94),
+            xycoords=ax.transAxes,
+            color="#000000",
+            alpha=1,
+            fontsize=18,
+            fontweight='bold')
+        ax.annotate(
+            'N = {}'.format(df.loc[df["hue"] == "#0072B2", :].shape[0]),
+            xy=(0.03, 0.90),
+            xycoords=ax.transAxes,
+            color="#0072B2",
+            alpha=1,
+            fontsize=18,
+            fontweight='bold')
+        ax.annotate(
+            'r = {:.2f}'.format(coef),
+            xy=(0.03, 0.86),
+            xycoords=ax.transAxes,
+            color="#000000",
+            alpha=1,
+            fontsize=18,
+            fontweight='bold')
+        ax.annotate(
+            'concordance = {:.0f}%'.format(concordance),
+            xy=(0.03, 0.82),
+            xycoords=ax.transAxes,
+            color="#000000",
+            alpha=1,
+            fontsize=18,
+            fontweight='bold')
+
+        ax.axhline(0, ls='--', color="#000000", zorder=-1)
+        ax.axvline(0, ls='--', color="#000000", zorder=-1)
+
+        ax.set_xlabel(xlabel,
+                      fontsize=20,
+                      fontweight='bold')
+        ax.set_ylabel(ylabel,
+                      fontsize=20,
+                      fontweight='bold')
+        ax.set_title(title,
+                     fontsize=25,
+                     fontweight='bold')
+
+        outpath = os.path.join(self.outdir, "main_eqtl_replication_plot_{}_vs_{}.png".format(self.d_name, self.r_name))
+        fig.savefig(outpath)
+        plt.close()
+        print("\tSaved: {}".format(outpath))
 
     def print_arguments(self):
         print("Arguments:")
